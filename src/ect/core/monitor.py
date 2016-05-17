2b621037303d16ae97c9698316d32bd6b1b30437
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-05-17 08:35:17 +0200

    an operation monitor

---
+++ src/ect/core/monitor.py
@@ -0,0 +1,9 @@
+class Monitor:
+    """
+    A monitor is used to both observe and control a running operation.
+    """
+    pass
+
+
+Monitor.NULL = Monitor()
+Monitor.NULL.__doc__ = 'A monitor that has no effect.'
