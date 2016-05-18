33c0e1cb119ab4ed9736ada049e9411dc598b969
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-05-18 14:48:31 +0200

    api doc + more tests

--- src/ect/core/util.py
+++ src/ect/core/util.py
@@ -1,4 +1,14 @@
-def qualified_name_to_object(qualified_name, default_module_name='builtins'):
+def qualified_name_to_object(qualified_name: str, default_module_name='builtins'):
+    """
+    Convert a fully qualified name into a Python object.
+    It is true that ``qualified_name_to_object(object_to_qualified_name(obj)) is obj``.
+
+    :param qualified_name: fully qualified name of the form [<module>'.'{<name>'.'}]<name>
+    :param default_module_name: default module name to be used if the name does not contain one
+    :return: the Python object
+    :raise ImportError: If the module could not be imported
+    :raise AttributeError: If the name could not be found
+    """
     parts = qualified_name.split('.')
 
     if len(parts) == 1:
@@ -13,7 +23,17 @@ def qualified_name_to_object(qualified_name, default_module_name='builtins'):
     return value
 
 
-def object_to_qualified_name(value, fail=False, default_module_name='builtins'):
+def object_to_qualified_name(value, fail=False, default_module_name='builtins') -> str:
+    """
+    Get the fully qualified name of a Python object.
+    It is true that ``qualified_name_to_object(object_to_qualified_name(obj)) is obj``.
+
+    :param value: some Python object
+    :param fail: raise ``ValueError``if name cannot be derived.
+    :param default_module_name: if this is the *value*'s module name, no module name will be returned.
+    :return: fully qualified name if it can be derived, otherwise``None`` if *fail* is ``False``.
+    :raise ValueError: if *fail* is``True`` and the name cannot be derived.
+    """
 
     module_name = value.__module__ if hasattr(value, '__module__') else None
     if module_name == default_module_name:
@@ -26,5 +46,3 @@ def object_to_qualified_name(value, fail=False, default_module_name='builtins'):
     if fail:
         raise ValueError("missing attribute '__name__'")
     return None
-
-
--- test/test_util.py
+++ test/test_util.py
@@ -9,9 +9,16 @@ class UtilTest(TestCase):
         self.assertEqual(object_to_qualified_name(float), 'float')
         self.assertEqual(object_to_qualified_name(TestCase), 'unittest.case.TestCase')
         self.assertEqual(object_to_qualified_name(ElementTree), 'xml.etree.ElementTree.ElementTree')
+        self.assertIs(object_to_qualified_name({}, fail=False), None)
+        with self.assertRaises(ValueError):
+            object_to_qualified_name({}, fail=True)
 
     def test_qualified_name_to_object(self):
         self.assertIs(qualified_name_to_object('float'), float)
         self.assertIs(qualified_name_to_object('builtins.float'), float)
         self.assertIs(qualified_name_to_object('unittest.case.TestCase'), TestCase)
         self.assertIs(qualified_name_to_object('xml.etree.ElementTree.ElementTree'), ElementTree)
+        with self.assertRaises(ImportError):
+            qualified_name_to_object('numpi.ndarray')
+        with self.assertRaises(AttributeError):
+            qualified_name_to_object('flaot')
