bf4f90d3de244ccf9465e1db9d5ccf98e8a2c33a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-05-18 22:33:37 +0200

    introduced _NullMonitor

--- src/ect/core/monitor.py
+++ src/ect/core/monitor.py
@@ -8,4 +8,10 @@ class Monitor:
     NULL = None
 
 
-Monitor.NULL = Monitor()
+class _NullMonitor(Monitor):
+    def __repr__(self):
+        return 'Monitor.NULL'
+
+
+#: Pass ``Monitor.NULL`` to ECT API functions that expect a monitor instead of passing ``None``.
+Monitor.NULL = _NullMonitor()
