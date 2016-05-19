e27964154acca0f69ea1556c4ccdc311952e58fa
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-05-19 14:26:44 +0200

    fixed test

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -4,11 +4,11 @@ from ect.core import plugin
 
 
 class PluginTest(TestCase):
-    def test_that_example_plugin_is_loaded(self):
+    def test_that_test_plugin_is_loaded(self):
         self.assertIsNotNone(plugin.REGISTRY)
         if 'test_plugin' in plugin.REGISTRY:
             # Note: if this fails, you should first do "python setup.py develop" in a terminal
-            self.assertEqual(plugin.REGISTRY['test_plugin'], ([], {}))
+            self.assertEqual(plugin.REGISTRY['test_plugin'], {'entry_point': 'test_plugin'})
         else:
             print('WARNING: PluginTest not performed, most likely because "python setup.py develop" has never been called.')
 
