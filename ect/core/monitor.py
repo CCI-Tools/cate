06a8b582c8e8d4362c7c1cfd7f239b7d05f8a1ce
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-05-24 09:22:10 +0200

    more apidoc

--- ect/core/monitor.py
+++ ect/core/monitor.py
@@ -3,12 +3,16 @@ Module Description
 ==================
 
 This module defines the :py:class:`Monitor` interface that is used by ECT to observe and control
-operations that support it.
+operations that support it. It also provides a number of useful default implementations:
+
+* :py:class:`ConsoleMonitor`: Prints progress output to the console.
+* :py:class:`BufferedMonitor`: Buffers progress output as a string so that e.g. a remote service can pick it up.
 
 
 Module Reference
 ================
 """
+
 
 class Monitor:
     """
@@ -27,3 +31,13 @@ class _NullMonitor(Monitor):
 
 #: Pass ``Monitor.NULL`` to ECT API functions that expect a monitor instead of passing ``None``.
 Monitor.NULL = _NullMonitor()
+
+
+class ConsoleMonitor(Monitor):
+    """A monitor that prints progress output to the console."""
+    pass
+
+
+class BufferedMonitor(Monitor):
+    """A monitor that buffers progress output as a string so that e.g. a remote service can pick it up."""
+    pass
