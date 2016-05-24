bed12def49cb4a3dc7604debd494ef48d4732e58
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2016-05-24 18:31:38 +0200

    work on io functions

--- ect/core/io.py
+++ ect/core/io.py
@@ -32,21 +32,42 @@ Module Reference
 ================
 """
 from typing import Sequence, Union
-
 from ect.core import Dataset
 
 
 class DataSource:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self._name = name
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
 
-def query_data_sources(catalogues=None, filter=None) -> Sequence[DataSource]:
-    return [DataSource("sst")]
+def _matches_filter(ds: DataSource, name_filter=None) -> bool:
+        if name_filter and not name_filter in ds.name:
+            return False
+        return True
+
+
+class Catalogue:
+    def __init__(self, *data_sources: DataSource):
+        self._data_sources = data_sources
+
+    def filter(self, name_filter: None) -> [DataSource]:
+        return [ds for ds in self._data_sources if _matches_filter(ds, name_filter=name_filter)]
+
+
+DEFAULT_CATALOGUE = Catalogue(DataSource("aerosol"), DataSource("ozone"))
+
+
+def query_data_sources(catalogues=DEFAULT_CATALOGUE, name_filter=None) -> Sequence[DataSource]:
+    if isinstance(catalogues, Catalogue):
+        catalogues = [catalogues]
+    results = []
+    for catalogue in catalogues:
+        results.extend(catalogue.filter(name_filter=name_filter))
+    return results
 
 
 def open_dataset(data_source: Union[DataSource, str], constraints=None) -> Dataset:
--- test/test_io.py
+++ test/test_io.py
@@ -5,10 +5,32 @@ class IOTest(TestCase):
 
     def test_query_data_sources(self):
         data_sources = io.query_data_sources()
+        self.assertIsNotNone(data_sources)
+        self.assertEqual(len(data_sources), 2)
+        self.assertEqual(data_sources[0].name, "aerosol")
+        self.assertEqual(data_sources[1].name, "ozone")
+
+    def test_query_data_sources_with_name(self):
+        data_sources = io.query_data_sources(name_filter="aero")
+        self.assertIsNotNone(data_sources)
         self.assertEqual(len(data_sources), 1)
-        self.assertEqual(data_sources[0].name, "sst")
+        self.assertEqual(data_sources[0].name, "aerosol")
+
+        data_sources = io.query_data_sources(name_filter="o")
+        self.assertIsNotNone(data_sources)
+        self.assertEqual(len(data_sources), 2)
+        self.assertEqual(data_sources[0].name, "aerosol")
+        self.assertEqual(data_sources[1].name, "ozone")
+
+    def test_query_data_sources_with_own_catalogue(self):
+        my_catalogue = io.Catalogue(io.DataSource("test"))
+        data_sources = io.query_data_sources(catalogues=my_catalogue)
+        self.assertIsNotNone(data_sources)
+        self.assertEqual(len(data_sources), 1)
+        self.assertEqual(data_sources[0].name, "test")
 
     # def test_open_dataset(self):
     #     ds1 = io.open_dataset(io.DataSource("sst"))
-    #     self.assertIsNotNone()
+    #     self.assertIsNotNone(ds1)
     #     ds2 = io.open_dataset("sst")
+    #     self.assertIsNotNone(ds2)
