5a53ea5b47e5e63b79ff5942c7527519d6c62488
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-06-01 16:00:01 +0200

    io revision

---
+++ ect/ds/esa_cci_portal_ftp.py
@@ -0,0 +1,11 @@
+
+def _add_default_file_catalogue():
+    ect_root_dir = 'ECT_DATA_ROOT'
+    if 'ECT_DATA_ROOT' in os.environ:
+        ect_root_dir = os.environ['ECT_DATA_ROOT']
+    json_data = pkgutil.get_data('ect.ds', 'esa_cci_portal_ftp.json')
+    cat = FileSetCatalogue.from_json(ect_root_dir, json_data.decode('utf-8'))
+    CATALOGUE_REGISTRY.add_catalogue('default', cat)
+
+
+_add_default_file_catalogue()
