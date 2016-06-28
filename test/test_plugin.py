080989964c8b5510f903c6f7fd86e5176ffeded4
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-06-28 13:28:56 +0200

    Tests don't print anymore

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,16 +1,26 @@
 from unittest import TestCase
 
 from ect.core import plugin
+from ect.core.util import fetch_std_streams
 
 
 class PluginTest(TestCase):
-    def test_that_test_plugin_is_loaded(self):
+    def test_that_test_plugins_are_loaded(self):
         self.assertIsNotNone(plugin.PLUGIN_REGISTRY)
-        if 'test_plugin' in plugin.PLUGIN_REGISTRY:
-            # Note: if this fails, you should first do "python setup.py develop" in a terminal
-            self.assertEqual(plugin.PLUGIN_REGISTRY['test_plugin'], {'entry_point': 'test_plugin'})
-        else:
-            print('WARNING: PluginTest not performed, most likely because "python setup.py develop" has never been called.')
+
+    def test_ect_init(self):
+        # Yes, this is really a silly test :)
+        # But this way we cover one more (empty) statement.
+        plugin.ect_init(True, False, a=1, b=2)
+
+    def test_error_reporting(self):
+        with fetch_std_streams() as (stdout, stderr):
+            plugin._report_plugin_error_msg('XXX')
+        self.assertEqual(stderr.getvalue(), 'error: XXX\n')
+
+        with fetch_std_streams() as (stdout, stderr):
+            plugin._report_plugin_exception('YYY')
+        self.assertEqual(stderr.getvalue(), 'error: YYY\n')
 
 
 
--- test/ui/test_cli.py
+++ test/ui/test_cli.py
@@ -1,81 +1,62 @@
 import sys
 import unittest
-from contextlib import contextmanager
-from io import StringIO
 from time import sleep
 
 from ect.core.monitor import Monitor
+from ect.core.util import fetch_std_streams
 from ect.ui import cli
-
-
-@contextmanager
-def fetch_std_streams():
-    sys.stdout.flush()
-    sys.stderr.flush()
-
-    old_stdout = sys.stdout
-    old_stderr = sys.stderr
-
-    sys.stdout = StringIO()
-    sys.stderr = StringIO()
-
-    try:
-        yield sys.stdout, sys.stderr
-    finally:
-        sys.stdout.flush()
-        sys.stderr.flush()
-
-        sys.stdout = old_stdout
-        sys.stderr = old_stderr
 
 
 class CliTest(unittest.TestCase):
     def test_noargs(self):
-        import sys
         sys.argv = []
-        status = cli.main()
-        self.assertEqual(status, 0)
+        with fetch_std_streams():
+            status = cli.main()
+            self.assertEqual(status, 0)
 
     def test_invalid_command(self):
-        status = cli.main(['pipo'])
-        self.assertEqual(status, 2)
+        with fetch_std_streams():
+            status = cli.main(['pipo'])
+            self.assertEqual(status, 2)
 
     def test_option_version(self):
-        status = cli.main(args=['--version'])
-        self.assertEqual(status, 0)
+        with fetch_std_streams():
+            status = cli.main(args=['--version'])
+            self.assertEqual(status, 0)
 
     def test_option_help(self):
-        status = cli.main(args=['--h'])
-        self.assertEqual(status, 0)
-        status = cli.main(args=['--help'])
-        self.assertEqual(status, 0)
+        with fetch_std_streams():
+            status = cli.main(args=['--h'])
+            self.assertEqual(status, 0)
+            status = cli.main(args=['--help'])
+            self.assertEqual(status, 0)
 
 
 class CliDataSourceCommandTest(unittest.TestCase):
     def test_command_ds_info(self):
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['ds', 'SOIL_MOISTURE_DAILY_FILES_ACTIVE_V02.2'])
             self.assertEqual(status, 0)
-        out1 = sout.getvalue()
+        out1 = stdout.getvalue()
         self.assertTrue('Base directory' in out1)
-        self.assertEqual(serr.getvalue(), '')
+        self.assertEqual(stderr.getvalue(), '')
 
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['ds', 'SOIL_MOISTURE_DAILY_FILES_ACTIVE_V02.2', '--info'])
             self.assertEqual(status, 0)
-        out2 = sout.getvalue()
+        out2 = stdout.getvalue()
 
         self.assertEqual(out1, out2)
 
     @unittest.skip(reason="skipped unless you want to debug data source synchronisation")
     def test_command_ds_sync(self):
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams():
             status = cli.main(args=['ds', 'SOIL_MOISTURE_DAILY_FILES_ACTIVE_V02.2', '--sync'])
             self.assertEqual(status, 0)
 
     @unittest.skip(reason="skipped unless you want to debug data source synchronisation")
     def test_command_ds_sync_with_period(self):
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams():
             status = cli.main(args=['ds', 'SOIL_MOISTURE_DAILY_FILES_ACTIVE_V02.2', '--sync', '--time', '2010-12'])
             self.assertEqual(status, 0)
 
@@ -91,9 +72,12 @@ class CliDataSourceCommandTest(unittest.TestCase):
 
         self.assertEqual(DataSourceCommand.parse_time_period('2010,2014'), (date(2010, 1, 1), date(2014, 12, 31)))
         self.assertEqual(DataSourceCommand.parse_time_period('2010-02,2010-09'), (date(2010, 2, 1), date(2010, 9, 30)))
-        self.assertEqual(DataSourceCommand.parse_time_period('2010-12,2011-12'), (date(2010, 12, 1), date(2011, 12, 31)))
-        self.assertEqual(DataSourceCommand.parse_time_period('2010-02-04,2019-02-04'), (date(2010, 2, 4), date(2019, 2, 4)))
-        self.assertEqual(DataSourceCommand.parse_time_period('2010-12-31,2010-01-06'), (date(2010, 12, 31), date(2010, 1, 6)))
+        self.assertEqual(DataSourceCommand.parse_time_period('2010-12,2011-12'),
+                         (date(2010, 12, 1), date(2011, 12, 31)))
+        self.assertEqual(DataSourceCommand.parse_time_period('2010-02-04,2019-02-04'),
+                         (date(2010, 2, 4), date(2019, 2, 4)))
+        self.assertEqual(DataSourceCommand.parse_time_period('2010-12-31,2010-01-06'),
+                         (date(2010, 12, 31), date(2010, 1, 6)))
 
         # errors
         self.assertEqual(DataSourceCommand.parse_time_period('2010-12-31,2010-01'), None)
@@ -103,28 +87,29 @@ class CliDataSourceCommandTest(unittest.TestCase):
         self.assertEqual(DataSourceCommand.parse_time_period('20L0-1-3-83,2010-01'), None)
 
     def test_command_run_no_args(self):
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['ds'])
             self.assertEqual(status, 2)
-        self.assertEqual(sout.getvalue(), '')
-        self.assertEqual(serr.getvalue(), "usage: ect ds [-h] [--time PERIOD] [--info] [--sync] DS_NAME [DS_NAME ...]\n"
-                                          "ect: ect ds: error: the following arguments are required: DS_NAME\n\n")
+        self.assertEqual(stdout.getvalue(), '')
+        self.assertEqual(stderr.getvalue(),
+                         "usage: ect ds [-h] [--time PERIOD] [--info] [--sync] DS_NAME [DS_NAME ...]\n"
+                         "ect: ect ds: error: the following arguments are required: DS_NAME\n\n")
 
 
 class CliRunCommandTest(unittest.TestCase):
     def test_command_run_with_unknown_op(self):
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['run', 'pipapo', 'lat=13.2', 'lon=52.9'])
             self.assertEqual(status, 1)
-        self.assertEqual(sout.getvalue(), '')
-        self.assertEqual(serr.getvalue(), "ect: error: command 'run': unknown operation 'pipapo'\n")
+        self.assertEqual(stdout.getvalue(), '')
+        self.assertEqual(stderr.getvalue(), "ect: error: command 'run': unknown operation 'pipapo'\n")
 
     def test_command_run_noargs(self):
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['run'])
             self.assertEqual(status, 2)
-        self.assertEqual(sout.getvalue(), '')
-        self.assertEqual(serr.getvalue(), "ect: error: command 'run' requires OP argument\n")
+        self.assertEqual(stdout.getvalue(), '')
+        self.assertEqual(stderr.getvalue(), "ect: error: command 'run' requires OP argument\n")
 
     def test_command_run_with_op(self):
         from ect.core.op import OP_REGISTRY as OP_REGISTRY
@@ -133,34 +118,34 @@ class CliRunCommandTest(unittest.TestCase):
 
         try:
             # Run without progress monitor
-            with fetch_std_streams() as (sout, serr):
+            with fetch_std_streams() as (stdout, stderr):
                 status = cli.main(args=['run', op_reg.meta_info.qualified_name, 'lat=13.2', 'lon=52.9'])
                 self.assertEqual(status, 0)
-            soutv = sout.getvalue()
+            soutv = stdout.getvalue()
             self.assertTrue('Running operation ' in soutv)
             self.assertTrue('lat=13.2 lon=52.9 method=nearest' in soutv)
             self.assertTrue('Output: [0.3, 0.25, 0.05, 0.4, 0.2, 0.1, 0.5]' in soutv)
-            self.assertEqual(serr.getvalue(), '')
+            self.assertEqual(stderr.getvalue(), '')
 
             # Run with progress monitor
-            with fetch_std_streams() as (sout, serr):
+            with fetch_std_streams() as (stdout, stderr):
                 status = cli.main(args=['run', '--monitor', op_reg.meta_info.qualified_name, 'lat=13.2', 'lon=52.9'])
                 self.assertEqual(status, 0)
-            soutv = sout.getvalue()
+            soutv = stdout.getvalue()
             self.assertTrue('Running operation ' in soutv)
             self.assertTrue('lat=13.2 lon=52.9 method=nearest' in soutv)
             self.assertTrue('Extracting timeseries data: started' in soutv)
             self.assertTrue('Extracting timeseries data:  33%' in soutv)
             self.assertTrue('Extracting timeseries data: done' in soutv)
             self.assertTrue('Output: [0.3, 0.25, 0.05, 0.4, 0.2, 0.1, 0.5]' in soutv)
-            self.assertEqual(serr.getvalue(), '')
+            self.assertEqual(stderr.getvalue(), '')
 
             # Run with invalid keyword
-            with fetch_std_streams() as (sout, serr):
+            with fetch_std_streams() as (stdout, stderr):
                 status = cli.main(args=['run', op_reg.meta_info.qualified_name, 'l*t=13.2', 'lon=52.9'])
                 self.assertEqual(status, 2)
-            self.assertEqual(sout.getvalue(), '')
-            self.assertEqual(serr.getvalue(), "ect: error: command 'run': keyword 'l*t' is not a valid identifier\n")
+            self.assertEqual(stdout.getvalue(), '')
+            self.assertEqual(stderr.getvalue(), "ect: error: command 'run': keyword 'l*t' is not a valid identifier\n")
 
         finally:
             OP_REGISTRY.remove_op(op_reg.operation, fail_if_not_exists=True)
@@ -176,93 +161,95 @@ class CliRunCommandTest(unittest.TestCase):
 
         try:
             # Run without progress monitor
-            with fetch_std_streams() as (sout, serr):
+            with fetch_std_streams() as (stdout, stderr):
                 status = cli.main(args=['run', workflow_file, 'lat=13.2', 'lon=52.9'])
                 self.assertEqual(status, 0)
-            soutv = sout.getvalue()
+            soutv = stdout.getvalue()
             self.assertTrue('Running workflow ' in soutv)
             self.assertTrue('lat=13.2 lon=52.9' in soutv)
             self.assertTrue('Output: return = [0.3, 0.25, 0.05, 0.4, 0.2, 0.1, 0.5]' in soutv)
-            self.assertEqual(serr.getvalue(), '')
+            self.assertEqual(stderr.getvalue(), '')
 
             # Run with progress monitor
-            with fetch_std_streams() as (sout, serr):
+            with fetch_std_streams() as (stdout, stderr):
                 status = cli.main(args=['run', '--monitor', workflow_file, 'lat=13.2', 'lon=52.9'])
                 self.assertEqual(status, 0)
-            soutv = sout.getvalue()
+            soutv = stdout.getvalue()
             self.assertTrue('Running workflow ' in soutv)
             self.assertTrue('lat=13.2 lon=52.9' in soutv)
             self.assertTrue('Extracting timeseries data: started' in soutv)
             self.assertTrue('Extracting timeseries data:  33%' in soutv)
             self.assertTrue('Extracting timeseries data: done' in soutv)
             self.assertTrue('Output: return = [0.3, 0.25, 0.05, 0.4, 0.2, 0.1, 0.5]' in soutv)
-            self.assertEqual(serr.getvalue(), '')
+            self.assertEqual(stderr.getvalue(), '')
 
         finally:
             OP_REGISTRY.remove_op(op_reg.operation, fail_if_not_exists=True)
 
     def test_command_run_help(self):
-        status = cli.main(args=['run', '-h'])
-        self.assertEqual(status, 0)
+        with fetch_std_streams():
+            status = cli.main(args=['run', '-h'])
+            self.assertEqual(status, 0)
 
-        status = cli.main(args=['run', '--help'])
-        self.assertEqual(status, 0)
+        with fetch_std_streams():
+            status = cli.main(args=['run', '--help'])
+            self.assertEqual(status, 0)
 
 
 class CliListCommandTest(unittest.TestCase):
     def test_command_list(self):
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['list'])
             self.assertEqual(status, 0)
-        self.assertIn('operation', sout.getvalue())
-        self.assertIn('found', sout.getvalue())
-        self.assertEqual(serr.getvalue(), '')
+        self.assertIn('operation', stdout.getvalue())
+        self.assertIn('found', stdout.getvalue())
+        self.assertEqual(stderr.getvalue(), '')
 
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['list', 'op'])
             self.assertEqual(status, 0)
-        self.assertIn('operation', sout.getvalue())
-        self.assertIn('found', sout.getvalue())
-        self.assertEqual(serr.getvalue(), '')
+        self.assertIn('operation', stdout.getvalue())
+        self.assertIn('found', stdout.getvalue())
+        self.assertEqual(stderr.getvalue(), '')
 
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['list', 'pi'])
             self.assertEqual(status, 0)
-        self.assertIn('plugin', sout.getvalue())
-        self.assertIn('found', sout.getvalue())
-        self.assertEqual(serr.getvalue(), '')
+        self.assertIn('plugin', stdout.getvalue())
+        self.assertIn('found', stdout.getvalue())
+        self.assertEqual(stderr.getvalue(), '')
 
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['list', 'ds'])
             self.assertEqual(status, 0)
-        self.assertIn('data source', sout.getvalue())
-        self.assertIn('found', sout.getvalue())
-        self.assertEqual(serr.getvalue(), '')
+        self.assertIn('data source', stdout.getvalue())
+        self.assertIn('found', stdout.getvalue())
+        self.assertEqual(stderr.getvalue(), '')
 
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['list', '--pattern', 'sst*', 'ds'])
             self.assertEqual(status, 0)
-        self.assertIn('data source', sout.getvalue())
-        self.assertIn('found', sout.getvalue())
-        self.assertEqual(serr.getvalue(), '')
+        self.assertIn('data source', stdout.getvalue())
+        self.assertIn('found', stdout.getvalue())
+        self.assertEqual(stderr.getvalue(), '')
 
 
 class CliLicenseCommandTest(unittest.TestCase):
     def test_command_license(self):
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['lic'])
             self.assertEqual(status, 0)
-        self.assertIn('GNU GENERAL PUBLIC LICENSE', sout.getvalue())
-        self.assertEqual(serr.getvalue(), '')
+        self.assertIn('GNU GENERAL PUBLIC LICENSE', stdout.getvalue())
+        self.assertEqual(stderr.getvalue(), '')
 
 
 class CliCopyrightCommandTest(unittest.TestCase):
     def test_command_copyright(self):
-        with fetch_std_streams() as (sout, serr):
+        with fetch_std_streams() as (stdout, stderr):
             status = cli.main(args=['cr'])
             self.assertEqual(status, 0)
-        self.assertIn('European Space Agency', sout.getvalue())
-        self.assertEqual(serr.getvalue(), '')
+        self.assertIn('European Space Agency', stdout.getvalue())
+        self.assertEqual(stderr.getvalue(), '')
 
 
 def timeseries(lat: float, lon: float, method: str = 'nearest', monitor=Monitor.NULL) -> list:
