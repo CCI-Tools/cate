f20d1b8d763d0f3d64d2b0eb4471b4d01ba18ed7
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-09-06 15:42:47 +0200

    improved exception msg

--- test/test_op.py
+++ test/test_op.py
@@ -334,7 +334,8 @@ class OpTest(TestCase):
 
         with self.assertRaises(ValueError) as cm:
             result = op_reg(x='A', y=3.)
-        self.assertEqual(str(cm.exception), "input 'x' for operation 'test.test_op.f' must be of type <class 'float'>")
+        self.assertEqual(str(cm.exception), "input 'x' for operation 'test.test_op.f' must be of type <class 'float'>, "
+                                            "but got <class 'str'>")
 
         with self.assertRaises(ValueError) as cm:
             result = op_reg(x=0.4)
