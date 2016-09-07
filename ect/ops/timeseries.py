9e1e103f34cc01837822834cb90def96104d261c
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-09-07 16:20:33 +0200

    refactoring

--- ect/ops/timeseries.py
+++ ect/ops/timeseries.py
@@ -18,7 +18,7 @@ from ect.core.op import op_input, op_return
 @op_input('lon', value_range=[-180, 180])
 @op_input('method', value_set=['nearest', 'ffill', 'bfill', None])
 @op_return(description='A timeseries dataset.')
-def timeseries(ds: xr.Dataset, lat: float, lon: float, method: str='nearest') -> xr.Dataset:
+def timeseries(ds: xr.Dataset, lat: float, lon: float, method: str = 'nearest') -> xr.Dataset:
     """
     Extract time-series from *ds* at given *lat*, *lon* position using interpolation *method*.
 
@@ -28,13 +28,14 @@ def timeseries(ds: xr.Dataset, lat: float, lon: float, method: str='nearest') ->
     :param method: One of ``nearest``, ``ffill``, ``bfill``.
     :return:
     """
-    wrapped_xarray = dataset
-    xarray_timeseries = _xarray_timeseries(wrapped_xarray, lat=lat, lon=lon, method=method)
-    return xarray_timeseries
+    lat_dim = _get_lat_dim_name(ds)
+    lon_dim = _get_lon_dim_name(ds)
+    indexers = {lat_dim: lat, lon_dim: lon}
+    return ds.sel(method=method, **indexers)
 
 
 @op_input('ds', description='A dataset from which to extract time series', required=True)
-@op_output('return', description='A timeseries dataset')
+@op_return(description='A timeseries dataset')
 def timeseries_mean(ds: xr.Dataset):
     """
     Extract spatial mean timeseries from the given dataset
@@ -43,28 +44,21 @@ def timeseries_mean(ds: xr.Dataset):
     :return: Time series dataset
     """
     # Expecting a harmonized dataset
-    reduce_along = {'dim':['lat','lon']}
+    reduce_along = {'dim': ['lat', 'lon']}
     retset = ds.mean(**reduce_along)
     return retset
 
 
-def _xarray_timeseries(dataset: xr.Dataset, lat: float, lon: float, method: str) -> xr.Dataset:
-    lat_dim = _get_lat_dim_name(dataset)
-    lon_dim = _get_lon_dim_name(dataset)
-    indexers = {lat_dim: lat, lon_dim: lon}
-    return ds.sel(method=method, **indexers)
+def _get_lon_dim_name(ds: xr.Dataset) -> str:
+    return _get_dim_name(ds, ['lon', 'longitude', 'long'])
 
 
-def _get_lon_dim_name(xarray: xr.Dataset) -> str:
-    return _get_dim_name(xarray, ['lon', 'longitude', 'long'])
+def _get_lat_dim_name(ds: xr.Dataset) -> str:
+    return _get_dim_name(ds, ['lat', 'latitude'])
 
 
-def _get_lat_dim_name(xarray: xr.Dataset) -> str:
-    return _get_dim_name(xarray, ['lat', 'latitude'])
-
-
-def _get_dim_name(xarray: xr.Dataset, possible_names) -> str:
+def _get_dim_name(ds: xr.Dataset, possible_names) -> str:
     for name in possible_names:
-        if name in xarray.dims:
+        if name in ds.dims:
             return name
     return None
