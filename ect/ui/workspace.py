92a717f73911177599d90749565f5be82f14e566
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-09-08 10:33:34 +0200

    added WorkspaceManager, WorkspaceError

--- ect/ui/workspace.py
+++ ect/ui/workspace.py
@@ -1,7 +1,69 @@
+import os
+import sys
+from abc import ABCMeta, abstractmethod
+
+from ect.core.op import OpMetaInfo
 from ect.core.workflow import Workflow, OpStep, NodePort
 
+WORKSPACE_DATA_DIR_NAME = '.ect-workspace'
+WORKSPACE_WORKFLOW_FILE_NAME = 'workflow.json'
 
-class Workspace(object):
+
+class WorkspaceError(Exception):
+    def __init__(self, cause, *args, **kwargs):
+        if isinstance(cause, Exception):
+            super(WorkspaceError, self).__init__(str(cause), *args, **kwargs)
+            _, _, traceback = sys.exc_info()
+            self.with_traceback(traceback)
+        elif isinstance(cause, str):
+            super(WorkspaceError, self).__init__(cause, *args, **kwargs)
+        else:
+            super(WorkspaceError, self).__init__(*args, **kwargs)
+        self._cause = cause
+
+    @property
+    def cause(self):
+        return self._cause
+
+
+class WorkspaceManager(metaclass=ABCMeta):
+    @abstractmethod
+    def init_workspace(self, base_dir: str):
+        pass
+
+
+class FileSystemWorkspaceManager(WorkspaceManager):
+    def init_workspace(self, base_dir: str = None):
+        try:
+            if not base_dir:
+                base_dir = os.path.abspath(os.curdir)
+            base_dir = os.path.abspath(base_dir)
+            if not os.path.isdir(base_dir):
+                os.mkdir(base_dir)
+            workspace_dir = os.path.join(base_dir, WORKSPACE_DATA_DIR_NAME)
+            workflow_file = os.path.join(workspace_dir, WORKSPACE_WORKFLOW_FILE_NAME)
+            if not os.path.isdir(workspace_dir):
+                os.mkdir(workspace_dir)
+            elif os.path.isfile(workflow_file):
+                raise WorkspaceError('workspace exists: %s' % base_dir)
+            workflow = self._new_workflow()
+            workflow.store(workflow_file)
+        except (IOError, OSError, FileExistsError) as e:
+            raise WorkspaceError(e)
+
+    @classmethod
+    def _new_workflow(cls):
+        workflow = Workflow(OpMetaInfo('workspace_workflow',
+                                       has_monitor=True,
+                                       header_dict=dict(description='A workflow used to record operations '
+                                                                    'performed in an ECT workspace. '
+                                                                    'By design, workspace '
+                                                                    'workflows have no inputs '
+                                                                    'and every step is an output.')))
+        return workflow
+
+
+class Workspace:
     def __init__(self, path):
         self._path = path
         self._workflow = Workflow('workspace-wf')
@@ -29,5 +91,3 @@ class Workspace(object):
         output_port = NodePort(self._workflow, name)
         output_port.source = step.output['return']
         self._workflow.output[name] = output_port
-
-
