07e783f6abb5e49a649819d8eaec5ec690b28af5
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-09-15 17:57:03 +0200

    ODP data access ongoing...

--- ect/ds/esa_cci_odp.py
+++ ect/ds/esa_cci_odp.py
@@ -37,41 +37,182 @@ and may be executed using
 Components
 ==========
 """
-
 import json
 import os
 import os.path
+import re
+import urllib.parse
 import urllib.request
 from datetime import datetime
-from typing import Sequence
+from typing import Sequence, Tuple
 
 import xarray as xr
 from ect.core.io import DATA_STORE_REGISTRY, DataStore, DataSource, Schema, TimeRange
 from ect.core.monitor import Monitor
 
-_ESGF_DATASET_URL = "https://esgf-index1.ceda.ac.uk/esg-search/search/?" \
-                    "type=Dataset&" \
-                    "offset=0&" \
-                    "limit=10000&" \
-                    "replica=false&" \
-                    "latest=true&" \
-                    "project=esacci&" \
-                    "format=application%2Fsolr%2Bjson"
-
-_ESGF_FILE_URL = "http://esgf-index1.ceda.ac.uk/esg-search/search/?" \
-                 "type=File&" \
-                 "offset=0&" \
-                 "limit=10000&" \
-                 "replica=false&" \
-                 "fields=url%2Ctitle&" \
-                 "dataset_id={dataset_id}&" \
-                 "format=application%2Fsolr%2Bjson"
+_ESGF_CEDA_URL = "https://esgf-index1.ceda.ac.uk/esg-search/search/"
 
 _DATA_SOURCES_DIR = os.path.expanduser(os.path.join('~', '.ect', 'data_stores'))
 _DATA_ROOT = os.path.join(_DATA_SOURCES_DIR, 'esa_cci_odp')
-_INDEX_CACHE_FILE = os.path.join(_DATA_ROOT, 'esgf-index-cache.json')
-_INDEX_TIMESTAMP_FILE = os.path.join(_DATA_ROOT, 'esgf-index-timestamp.txt')
-_INDEX_TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+_TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+_RE_TO_DATETIME_FORMATS = patterns = [(re.compile(14 * '\\d'), '%Y%m%d%H%M%S'),
+                                      (re.compile(12 * '\\d'), '%Y%m%d%H%M'),
+                                      (re.compile(8 * '\\d'), '%Y%m%d'),
+                                      (re.compile(6 * '\\d'), '%Y%m'),
+                                      (re.compile(4 * '\\d'), '%Y')]
+
+
+def set_default_data_store():
+    """
+    Defines the ESA CCI ODP data store and makes it the default data store.
+
+    All data sources of the FTP data store are read from a JSON file ``esa_cci_ftp.json`` contained in this package.
+    This JSON file has been generated from a scan of the entire FTP tree.
+    """
+    DATA_STORE_REGISTRY.add_data_store('default', EsaCciOdpDataStore())
+
+
+def _find_datetime_format(filename: str) -> Tuple[str, int, int]:
+    for regex, time_format in _RE_TO_DATETIME_FORMATS:
+        searcher = regex.search(filename)
+        if searcher:
+            p1, p2 = searcher.span()
+            return time_format, p1, p2
+    return None, -1, -1
+
+
+def _fetch_solr_json(base_url, query_args, offset=0, limit=10000, timeout=10):
+    """
+    Return JSON value read from paginated Solr web-service.
+    """
+    new_offset = offset
+    combined_json_dict = None
+    combined_num_found = 0
+    while True:
+        paging_query_args = dict(query_args or {})
+        paging_query_args.update(offset=new_offset, limit=limit, format='application/solr+json')
+        url = base_url + '?' + urllib.parse.urlencode(paging_query_args)
+        with urllib.request.urlopen(url, timeout=timeout) as response:
+            json_text = response.read()
+            json_dict = json.loads(json_text.decode('utf-8'))
+            num_found = json_dict.get('response', {}).get('numFound', 0)
+            if not combined_json_dict:
+                combined_json_dict = json_dict
+                if num_found < limit:
+                    break
+            else:
+                if num_found > 0:
+                    combined_num_found += num_found
+                    docs = json_dict.get('response', {}).get('docs', [])
+                    combined_json_dict.get('response', {}).get('docs', []).append(docs)
+                    combined_json_dict.get('response', {})['numFound'] = combined_num_found
+                    if num_found < limit:
+                        break
+                else:
+                    break
+    return combined_json_dict
+
+
+def _load_or_fetch_json(fetch_json_function,
+                        fetch_json_args: list = None,
+                        fetch_json_kwargs: dict = None,
+                        cache_used: bool = False,
+                        cache_dir: str = None,
+                        cache_json_filename: str = None,
+                        cache_timestamp_filename: str = None,
+                        cache_expiration_days: float = 1.0) -> Sequence:
+    """
+    Return (JSON) value of fetch_json_function or return value of a cached JSON file.
+    """
+    json_obj = None
+
+    if cache_used:
+        if cache_dir is None:
+            raise ValueError('if cache_used argument is True, cache_dir argument must not be None')
+        if cache_json_filename is None:
+            raise ValueError('if cache_used argument is True, cache_json_filename argument must not be None')
+        if cache_timestamp_filename is None:
+            raise ValueError('if cache_used argument is True, cache_timestamp_filename argument must not be None')
+        if cache_expiration_days is None:
+            raise ValueError('if cache_used argument is True, cache_expiration_days argument must not be None')
+
+        cache_json_file = os.path.join(cache_dir, cache_json_filename)
+        cache_timestamp_file = os.path.join(cache_dir, cache_timestamp_filename)
+
+        timestamp = datetime(year=2000, month=1, day=1)
+        if os.path.exists(cache_timestamp_file):
+            with open(cache_timestamp_file) as fp:
+                timestamp_text = fp.read()
+                timestamp = datetime.strptime(timestamp_text, _TIMESTAMP_FORMAT)
+
+        time_diff = datetime.now() - timestamp
+        time_diff_days = time_diff.days + time_diff.seconds / 3600. / 24.
+        if time_diff_days < cache_expiration_days:
+            if os.path.exists(cache_json_file):
+                with open(cache_json_file) as fp:
+                    json_text = fp.read()
+                    json_obj = json.loads(json_text)
+
+    if json_obj is None:
+        # noinspection PyArgumentList
+        json_obj = fetch_json_function(*(fetch_json_args or []), **(fetch_json_kwargs or {}))
+        if cache_used:
+            os.makedirs(cache_dir, exist_ok=True)
+            # noinspection PyUnboundLocalVariable
+            with open(cache_json_file, 'w') as fp:
+                fp.write(json.dumps(json_obj, indent='  '))
+            # noinspection PyUnboundLocalVariable
+            with open(cache_timestamp_file, 'w') as fp:
+                fp.write(datetime.utcnow().strftime(_TIMESTAMP_FORMAT))
+
+    return json_obj
+
+
+def _fetch_file_list_json(dataset_id: str, dataset_query_id: str):
+    file_index_json_dict = _fetch_solr_json(_ESGF_CEDA_URL,
+                                            dict(type='File',
+                                                 fields='url,title',
+                                                 dataset_id=dataset_query_id,
+                                                 replica='false',
+                                                 latest='true',
+                                                 project='esacci'))
+
+    if not isinstance(file_index_json_dict, dict):
+        return None
+
+    file_list = []
+    docs = file_index_json_dict.get('response', {}).get('docs', [])
+    time_info = None
+    for doc in docs:
+        url_rec_list = doc.get('url', [])
+        for url_rec in url_rec_list:
+            url, mime_type, service_type = url_rec.split('|')
+            if mime_type == 'application/netcdf' and service_type == 'HTTPServer':
+                filename = doc.get('title', None)
+                if not filename:
+                    filename = os.path.basename(urllib.parse.urlparse(url)[2])
+                if filename in file_list:
+                    raise ValueError('filename %s already seen in dataset %s' % (filename, dataset_id))
+                if not time_info:
+                    time_info = _find_datetime_format(filename)
+                # Start time will be extracted from filename using time_info
+                start_time = None
+                # We also reserve an end_time field, just in case.
+                end_time = None
+                if time_info:
+                    time_format, p1, p2 = time_info
+                    start_time = datetime.strptime(filename[p1:p2], time_format)
+                    # Convert back to text, so we can JSON-encode it
+                    start_time = datetime.strftime(start_time, _TIMESTAMP_FORMAT)
+                file_list.append([filename, start_time, end_time, url])
+                break
+
+    def pick_start_time(file_info_rec):
+        return file_info_rec[1]
+
+    return sorted(file_list, key=pick_start_time)
 
 
 class EsaCciOdpDataStore(DataStore):
@@ -82,8 +223,16 @@ class EsaCciOdpDataStore(DataStore):
         super(EsaCciOdpDataStore, self).__init__()
         self._index_cache_used = index_cache_used
         self._index_cache_expiration_days = index_cache_expiration_days
-        self._index_cache_json_dict = index_cache_json_dict
+        self._index_json_dict = index_cache_json_dict
         self._data_sources = []
+
+    @property
+    def index_cache_used(self):
+        return self._index_cache_used
+
+    @property
+    def index_cache_expiration_days(self):
+        return self._index_cache_expiration_days
 
     def query(self, name: str = None) -> Sequence['DataSource']:
         self._init_data_sources()
@@ -99,65 +248,43 @@ class EsaCciOdpDataStore(DataStore):
         if self._data_sources:
             return
         self._load_index()
-        response = self._index_cache_json_dict.get('response')
-        num_found = response.get('numFound')
-        docs = response.get('docs')
+        docs = self._index_json_dict.get('response', {}).get('docs', [])
         self._data_sources = []
         for doc in docs:
-            master_id = doc.get('master_id', None)
-            self._data_sources.append(OdpDataSource(self, master_id, doc))
+            self._data_sources.append(EsaCciOdpDataSource(self, doc))
 
     def _load_index(self) -> str:
-        if self._index_cache_json_dict is not None:
-            return
-
-        if self._index_cache_used:
-            timestamp = datetime(year=2000, month=1, day=1)
-            if os.path.exists(_INDEX_TIMESTAMP_FILE):
-                with open(_INDEX_TIMESTAMP_FILE) as fp:
-                    timestamp_text = fp.read()
-                    timestamp = datetime.strptime(timestamp_text, _INDEX_TIMESTAMP_FORMAT)
-
-            time_diff = datetime.now() - timestamp
-            time_diff_days = time_diff.days + time_diff.seconds / 3600. / 24.
-            if time_diff_days < self._index_cache_expiration_days:
-                if os.path.exists(_INDEX_CACHE_FILE):
-                    with open(_INDEX_CACHE_FILE) as fp:
-                        json_text = fp.read()
-                        self._index_cache_json_dict = json.loads(json_text)
-
-        if self._index_cache_json_dict is None:
-            with urllib.request.urlopen(_ESGF_DATASET_URL, timeout=10) as response:
-                json_text = response.read()
-                json_text = json_text.decode('utf-8')
-                self._index_cache_json_dict = json.loads(json_text)
-
-                if self._index_cache_used:
-                    os.makedirs(_DATA_ROOT, exist_ok=True)
-                    with open(_INDEX_CACHE_FILE, 'w') as fp:
-                        fp.write(json_text)
-                    with open(_INDEX_TIMESTAMP_FILE, 'w') as fp:
-                        fp.write(datetime.utcnow().strftime(_INDEX_TIMESTAMP_FORMAT))
+        self._index_json_dict = _load_or_fetch_json(_fetch_solr_json,
+                                                    fetch_json_args=[_ESGF_CEDA_URL, dict(type='Dataset',
+                                                                                          replica='false',
+                                                                                          latest='true',
+                                                                                          project='esacci')],
+                                                    cache_used=self._index_cache_used,
+                                                    cache_dir=_DATA_ROOT,
+                                                    cache_json_filename='dataset-list.json',
+                                                    cache_timestamp_filename='dataset-list-timestamp.json',
+                                                    cache_expiration_days=self._index_cache_expiration_days)
 
 
-class OdpDataSource(DataSource):
+class EsaCciOdpDataSource(DataSource):
     def __init__(self,
-                 data_store: DataStore,
-                 master_id: str,
+                 data_store: EsaCciOdpDataStore,
                  json_dict: dict,
                  schema: Schema = None):
-        super(OdpDataSource, self).__init__()
-        self._master_id = master_id
+        super(EsaCciOdpDataSource, self).__init__()
+        self._master_id = json_dict.get('master_id', None)
+        self._dataset_id = json_dict.get('id', None)
         self._data_store = data_store
         self._json_dict = json_dict
         self._schema = schema
+        self._file_list = None
 
     @property
     def name(self) -> str:
         return self._master_id
 
     @property
-    def data_store(self) -> DataStore:
+    def data_store(self) -> EsaCciOdpDataStore:
         return self._data_store
 
     @property
@@ -203,7 +330,7 @@ class OdpDataSource(DataSource):
 
     def find_url(self, desired_service='HTTP'):
         for url_service in self._json_dict.get('url', []):
-            parts = url_service.rsplit('|', maxsplit=1)
+            parts = url_service.split('|')
             if len(parts) == 2:
                 url, service = parts
                 if service == desired_service:
@@ -211,24 +338,35 @@ class OdpDataSource(DataSource):
         return None
 
     def sync(self, time_range: TimeRange = (None, None), monitor: Monitor = Monitor.NULL):
-        http_url = self.find_url()
-        raise NotImplementedError
+        self._init_file_list()
+
+        import urllib.request
+        urllib.request.urlretrieve()
+        return None
 
     def open_dataset(self, time_range: TimeRange = None) -> xr.Dataset:
-        raise NotImplementedError
+        self._init_file_list()
+        return None
+
+    def _init_file_list(self) -> str:
+        if self._file_list:
+            return
+
+        dataset_dir = os.path.join(_DATA_ROOT, self._master_id)
+        file_list = _load_or_fetch_json(_fetch_file_list_json,
+                                        fetch_json_args=[self._master_id, self._dataset_id],
+                                        cache_used=self._data_store.index_cache_used,
+                                        cache_dir=dataset_dir,
+                                        cache_json_filename='file-list.json',
+                                        cache_timestamp_filename='file-list-timestamp.txt',
+                                        cache_expiration_days=self._data_store.index_cache_expiration_days)
+        for file_rec in file_list:
+            # Convert start_time string to datetime object
+            file_rec[1] = datetime.strptime(file_rec[1], _TIMESTAMP_FORMAT)
+        self._file_list = file_list
+
+    def __str__(self):
+        return self.name
 
     def _repr_html_(self):
         return self.info_string
-
-    def __str__(self):
-        return self.info_string
-
-
-def set_default_data_store():
-    """
-    Defines the ESA CCI ODP data store and makes it the default data store.
-
-    All data sources of the FTP data store are read from a JSON file ``esa_cci_ftp.json`` contained in this package.
-    This JSON file has been generated from a scan of the entire FTP tree.
-    """
-    DATA_STORE_REGISTRY.add_data_store('default', EsaCciOdpDataStore())
--- test/ds/test_esa_cci_odp.py
+++ test/ds/test_esa_cci_odp.py
@@ -1,14 +1,20 @@
 import json
+import os
 import os.path
 import unittest
 
-from ect.ds.esa_cci_odp import EsaCciOdpDataStore
+from ect.ds.esa_cci_odp import EsaCciOdpDataStore, _find_datetime_format
 
 
-@unittest.skip(reason="skipped unless you want to debug index cache management")
+@unittest.skip(reason='Because it writes a lot of files')
+# @unittest.skipUnless(condition=os.environ.get('ECT_ODP_TEST', None), reason="skipped unless you set ECT_ODP_TEST=1")
 class EsaCciOdpDataStoreIndexCacheTest(unittest.TestCase):
     def test_index_cache(self):
         self.data_store = EsaCciOdpDataStore(index_cache_used=True, index_cache_expiration_days=1.0e-6)
+        data_sources = self.data_store.query()
+        self.assertIsNotNone(data_sources)
+        for data_source in data_sources:
+            data_source.sync()
 
 
 def _create_test_data_store():
@@ -55,7 +61,7 @@ class EsaCciOdpDataSourceTest(unittest.TestCase):
 
     def test_info_string(self):
         self.maxDiff = None
-        #print(self.data_source.info_string)
+        # print(self.data_source.info_string)
         self.assertEqual(self.data_source.info_string,
                          'Data source "esacci.OC.day.L3S.K_490.multi-sensor.multi-platform.MERGED.1-0.r2"\n'
                          '===============================================================================\n'
@@ -74,3 +80,72 @@ class EsaCciOdpDataSourceTest(unittest.TestCase):
                          'size:                   35338081094\n'
                          'time_frequency:         day\n'
                          'version:                20160704')
+
+    def assert_tf(self, filename: str, expected_time_format: str):
+        time_format, p1, p2 = _find_datetime_format(filename)
+        self.assertEqual(time_format, expected_time_format)
+
+    def test_time_filename_patterns(self):
+        self.assert_tf('20020730174408-ESACCI-L3U_GHRSST-SSTskin-AATSR-LT-v02.0-fv01.1.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('19911107054700-ESACCI-L2P_GHRSST-SSTskin-AVHRR12_G-LT-v02.0-fv01.0.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-SEAICE-L4-SICONC-SSMI-NH25kmEASE2-19920610-fv01.11.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-SEAICE-L4-SICONC-SSMI-SH25kmEASE2-20000101-20001231-fv01.11.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-SEAICE-L4-SICONC-AMSR-NH25kmEASE2-20070204-fv01.11.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-SEAICE-L4-SICONC-AMSR-SH25kmEASE2-20040427-fv01.11.nc', '%Y%m%d')
+        self.assert_tf('19921018120000-ESACCI-L4_GHRSST-SSTdepth-OSTIA-GLOB_LT-v02.0-fv01.0.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('19940104120000-ESACCI-L4_GHRSST-SSTdepth-OSTIA-GLOB_LT-v02.0-fv01.1.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-OZONE-L3S-TC-MERGED-DLR_1M-20090301-fv0100.nc', '%Y%m%d')
+        self.assert_tf('20070328-ESACCI-L3U_CLOUD-CLD_PRODUCTS-AVHRR_NOAA-15-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('20091002-ESACCI-L3U_CLOUD-CLD_PRODUCTS-AVHRR_NOAA-16-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('20090729-ESACCI-L3U_CLOUD-CLD_PRODUCTS-AVHRR_NOAA-18-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('20070410-ESACCI-L3U_CLOUD-CLD_PRODUCTS-AVHRR_NOAA-17-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-K_490-MERGED-1D_DAILY_4km_SIN_PML_KD490_Lee-20000129-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-K_490-MERGED-1D_DAILY_4km_GEO_PML_KD490_Lee-19980721-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OZONE-L3-NP-MERGED-KNMI-200812-fv0002.nc', '%Y%m')
+        self.assert_tf('ESACCI-OC-L3S-CHLOR_A-MERGED-1D_DAILY_4km_GEO_PML_OC4v6-19971130-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-CHLOR_A-MERGED-1D_DAILY_4km_SIN_PML_OC4v6-19980625-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('200903-ESACCI-L3C_CLOUD-CLD_PRODUCTS-AVHRR_NOAA-15-fv1.0.nc', '%Y%m')
+        self.assert_tf('ESACCI-GHG-L2-CH4-GOSAT-SRPR-20100501-fv1.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-GHG-L2-CH4-GOSAT-SRPR-20091201-fv1.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-GHG-L2-CO2-GOSAT-SRFP-20101220-fv1.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-GHG-L2-CH4-GOSAT-SRFP-20100109-fv1.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-GHG-L2-CO2-GOSAT-SRFP-20090527-fv1.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-GHG-L2-CH4-GOSAT-SRFP-20100714-fv1.nc', '%Y%m%d')
+        self.assert_tf('20090616-ESACCI-L3U_CLOUD-CLD_PRODUCTS-MODIS_TERRA-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('20070717-ESACCI-L3U_CLOUD-CLD_PRODUCTS-MODIS_AQUA-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-8D_DAILY_4km_GEO_PML_OC4v6_QAA-19971211-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-8D_DAILY_4km_SIN_PML_OC4v6_QAA-20080921-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-1M_MONTHLY_4km_SIN_PML_OC4v6_QAA-200906-fv1.0.nc', '%Y%m')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-1M_MONTHLY_4km_GEO_PML_OC4v6_QAA-200707-fv1.0.nc', '%Y%m')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-1Y_YEARLY_4km_GEO_PML_OC4v6_QAA-2005-fv1.0.nc', '%Y')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-1Y_YEARLY_4km_GEO_PML_OC4v6_QAA-2003-fv1.0.nc', '%Y')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-8D_DAILY_4km_GEO_PML_OC4v6_QAA-19970914-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-IOP-MERGED-1D_DAILY_4km_GEO_PML_QAA-19970915-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-IOP-MERGED-1D_DAILY_4km_GEO_PML_QAA-19980724-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('20020822103843-ESACCI-L3U_GHRSST-SSTskin-AATSR-LT-v02.0-fv01.0.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-OZONE-L3S-TC-MERGED-DLR_1M-19980301-fv0100.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-SOILMOISTURE-L3S-SSMV-COMBINED-19781120000000-fv02.1.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-SOILMOISTURE-L3S-SSMV-PASSIVE-19791011000000-fv02.1.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-SOILMOISTURE-L3S-SSMV-PASSIVE-19790519000000-fv02.2.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-SOILMOISTURE-L3S-SSMS-ACTIVE-19911026000000-fv02.1.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-SOILMOISTURE-L3S-SSMS-ACTIVE-19911010000000-fv02.2.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-SEALEVEL-IND-MSL-MERGED-20151104000000-fv01.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-SEALEVEL-IND-MSLAMPH-MERGED-20151104000000-fv01.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-SEALEVEL-IND-MSLTR-MERGED-20151104000000-fv01.nc', '%Y%m%d%H%M%S')
+        self.assert_tf('ESACCI-OC-L3S-RRS-MERGED-1D_DAILY_4km_GEO_PML_RRS-19980418-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-RRS-MERGED-1D_DAILY_4km_SIN_PML_RRS-19980925-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('200811-ESACCI-L3C_CLOUD-CLD_PRODUCTS-AVHRR_NOAA-18-fv1.0.nc', '%Y%m')
+        self.assert_tf('200704-ESACCI-L3C_CLOUD-CLD_PRODUCTS-AVHRR_NOAA-16-fv1.0.nc', '%Y%m')
+        self.assert_tf('200811-ESACCI-L3C_CLOUD-CLD_PRODUCTS-AVHRR_NOAA-17-fv1.0.nc', '%Y%m')
+        self.assert_tf('200712-ESACCI-L3C_CLOUD-CLD_PRODUCTS-MODIS_TERRA-fv1.0.nc', '%Y%m')
+        self.assert_tf('200902-ESACCI-L3C_CLOUD-CLD_PRODUCTS-MODIS_AQUA-fv1.0.nc', '%Y%m')
+        self.assert_tf('200706-ESACCI-L3S_CLOUD-CLD_PRODUCTS-MODIS_MERGED-fv1.0.nc', '%Y%m')
+        self.assert_tf('200901-ESACCI-L3S_CLOUD-CLD_PRODUCTS-AVHRR_MERGED-fv1.0.nc', '%Y%m')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-1M_MONTHLY_4km_GEO_PML_OC4v6_QAA-200505-fv1.0.nc', '%Y%m')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-1D_DAILY_4km_SIN_PML_OC4v6_QAA-19980720-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-1D_DAILY_4km_GEO_PML_OC4v6_QAA-19990225-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-8D_DAILY_4km_GEO_PML_OC4v6_QAA-19990407-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('ESACCI-OC-L3S-OC_PRODUCTS-MERGED-1D_DAILY_4km_GEO_PML_OC4v6_QAA-19970915-fv1.0.nc', '%Y%m%d')
+        self.assert_tf('20060107-ESACCI-L4_FIRE-BA-MERIS-fv4.1.nc', '%Y%m%d')
+
+
