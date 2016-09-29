f3b679948cecf76f849cd21b3777d6033d31aed8
Janis Gailis <gailis@stcorp.no>
2016-09-29 10:54:14 +0200

    Adding tests for new-old timeseries_point

--- ect/ops/timeseries.py
+++ ect/ops/timeseries.py
@@ -34,6 +34,7 @@ import xarray as xr
 from ect.core.op import op_input, op
 from ect.core.util import to_list
 from ect.ops.select import select_var
+import fnmatch
 
 
 @op(tags=['timeseries', 'temporal', 'point'])
--- test/ops/test_timeseries.py
+++ test/ops/test_timeseries.py
@@ -5,7 +5,6 @@ Tests for timeseries operations
 from unittest import TestCase
 
 import numpy as np
-import pandas as pd
 import xarray as xr
 
 from ect.ops.timeseries import tseries_point, tseries_mean
@@ -15,40 +14,46 @@ class TimeSeriesTest(TestCase):
     def test_tseries_point(self):
         # Test general functionality
         dataset = xr.Dataset({
-            'abs': (['lat', 'lon', 'time'], np.ones([4,8,6])),
-            'bbs': (['lat', 'lon', 'time'], np.ones([4,8,6])),
+            'abs': (['lat', 'lon', 'time'], np.ones([4, 8, 6])),
+            'bbs': (['lat', 'lon', 'time'], np.ones([4, 8, 6])),
             'lat': np.linspace(-67.5, 67.5, 4),
             'lon': np.linspace(-157.5, 157.5, 8),
-            'time': ['2000-01-01','2000-02-01','2000-03-01','2000-04-01','2000-05-01','2000-06-01']})
+            'time': ['2000-01-01', '2000-02-01', '2000-03-01', '2000-04-01',
+                     '2000-05-01', '2000-06-01']})
 
         actual = tseries_point(dataset, lat=5, lon=10, var='*bs')
         expected = xr.Dataset({
-            'abs_ts_5_10': (['time'], np.ones(6)),
-            'bbs_ts_5_10': (['time'], np.ones(6)),
-            'time': ['2000-01-01','2000-02-01','2000-03-01','2000-04-01','2000-05-01','2000-06-01']})
-        actual = actual.drop(['lat', 'lon'])
+            'abs': (['time'], np.ones(6)),
+            'bbs': (['time'], np.ones(6)),
+            'lat': 22.5,
+            'lon': 22.5,
+            'time': ['2000-01-01', '2000-02-01', '2000-03-01', '2000-04-01',
+                     '2000-05-01', '2000-06-01']})
+        expected = expected.set_coords(['lat', 'lon'])
         self.assertDatasetEqual(expected, actual)
 
         actual = tseries_point(dataset, lat=5, lon=10, var='')
-        self.assertDatasetEqual(actual, dataset)
+        self.assertDatasetEqual(expected, actual)
 
     def test_tseries_mean(self):
         # Test general functionality
         dataset = xr.Dataset({
-            'abs': (['lat', 'lon', 'time'], np.ones([4,8,6])),
-            'bbs': (['lat', 'lon', 'time'], np.ones([4,8,6])),
+            'abs': (['lat', 'lon', 'time'], np.ones([4, 8, 6])),
+            'bbs': (['lat', 'lon', 'time'], np.ones([4, 8, 6])),
             'lat': np.linspace(-67.5, 67.5, 4),
             'lon': np.linspace(-157.5, 157.5, 8),
-            'time': ['2000-01-01','2000-02-01','2000-03-01','2000-04-01','2000-05-01','2000-06-01']})
+            'time': ['2000-01-01', '2000-02-01', '2000-03-01', '2000-04-01',
+                     '2000-05-01', '2000-06-01']})
         actual = tseries_mean(dataset, var='*bs')
         expected = xr.Dataset({
-            'abs': (['lat', 'lon', 'time'], np.ones([4,8,6])),
-            'bbs': (['lat', 'lon', 'time'], np.ones([4,8,6])),
+            'abs': (['lat', 'lon', 'time'], np.ones([4, 8, 6])),
+            'bbs': (['lat', 'lon', 'time'], np.ones([4, 8, 6])),
             'abs_ts_mean': (['time'], np.ones([6])),
             'bbs_ts_mean': (['time'], np.ones([6])),
             'lat': np.linspace(-67.5, 67.5, 4),
             'lon': np.linspace(-157.5, 157.5, 8),
-            'time': ['2000-01-01','2000-02-01','2000-03-01','2000-04-01','2000-05-01','2000-06-01']})
+            'time': ['2000-01-01', '2000-02-01', '2000-03-01', '2000-04-01',
+                     '2000-05-01', '2000-06-01']})
         self.assertDatasetEqual(expected, actual)
 
         # Test damage control
@@ -56,6 +61,7 @@ class TimeSeriesTest(TestCase):
         self.assertDatasetEqual(actual, dataset)
 
     def assertDatasetEqual(self, expected, actual):
-        # this method is functionally equivalent to `assert expected == actual`, but it
-        # checks each aspect of equality separately for easier debugging
+        # this method is functionally equivalent to
+        # `assert expected == actual`, but it checks each aspect of
+        # equality separately for easier debugging
         assert expected.equals(actual), (expected, actual)
