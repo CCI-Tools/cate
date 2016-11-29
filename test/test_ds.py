7f1f8fa33f0876a0132e7f49d43f4045b6ce4f7a
Krzysztof (Chris) Bernat <chris.bernat@telespazio.com>
2016-11-29 10:46:42 +0000

    fixed DS api tests

--- test/test_ds.py
+++ test/test_ds.py
@@ -2,39 +2,39 @@ from typing import Sequence
 from unittest import TestCase
 
 import xarray as xr
-import cate.core.ds as io
+import cate.core.ds as ds
 
 
-class SimpleDataStore(io.DataStore):
-    def __init__(self, name: str, data_sources: Sequence[io.DataSource]):
+class SimpleDataStore(ds.DataStore):
+    def __init__(self, name: str, data_sources: Sequence[ds.DataSource]):
         super().__init__(name)
         self._data_sources = list(data_sources)
 
-    def query(self, name=None) -> Sequence[io.DataSource]:
+    def query(self, name=None) -> Sequence[ds.DataSource]:
         return [ds for ds in self._data_sources if ds.matches_filter(name)]
 
     def _repr_html_(self):
         return ''
 
 
-class SimpleDataSource(io.DataSource):
+class SimpleDataSource(ds.DataSource):
     def __init__(self, name: str):
         self._name = name
         self._data_store = None
 
     @property
-    def data_store(self) -> io.DataStore:
+    def data_store(self) -> ds.DataStore:
         return self.data_store
 
     @property
-    def schema(self) -> io.Schema:
+    def schema(self) -> ds.Schema:
         return None
 
     @property
     def name(self) -> str:
         return self._name
 
-    def open_dataset(self, time_range=None):
+    def open_dataset(self, time_range=None, protocol: str=None):
         return None
 
     def __repr__(self):
@@ -49,7 +49,7 @@ class InMemoryDataSource(SimpleDataSource):
         super(InMemoryDataSource, self).__init__("in_memory")
         self._data = data
 
-    def open_dataset(self, time_range=None) -> xr.Dataset:
+    def open_dataset(self, time_range=None, protocol: str=None) -> xr.Dataset:
         return xr.Dataset({'a':self._data})
 
     def __repr__(self):
@@ -72,36 +72,36 @@ class IOTest(TestCase):
         self.TEST_DATA_STORE_SST = SimpleDataStore('test_sst', [self.DS_SST])
 
     def test_query_data_sources_default_data_store(self):
-        size_before = len(io.DATA_STORE_REGISTRY)
-        orig_stores = list(io.DATA_STORE_REGISTRY.get_data_stores())
+        size_before = len(ds.DATA_STORE_REGISTRY)
+        orig_stores = list(ds.DATA_STORE_REGISTRY.get_data_stores())
         try:
-            io.DATA_STORE_REGISTRY._data_stores.clear()
-            self.assertEqual(0, len(io.DATA_STORE_REGISTRY))
+            ds.DATA_STORE_REGISTRY._data_stores.clear()
+            self.assertEqual(0, len(ds.DATA_STORE_REGISTRY))
 
             from cate.ds.esa_cci_ftp import set_default_data_store as set_default_data_store_ftp
             set_default_data_store_ftp()
-            self.assertEqual(1, len(io.DATA_STORE_REGISTRY))
+            self.assertEqual(1, len(ds.DATA_STORE_REGISTRY))
 
-            data_sources = io.query_data_sources()
+            data_sources = ds.query_data_sources()
             self.assertIsNotNone(data_sources)
             self.assertEqual(len(data_sources), 98)
             self.assertEqual(data_sources[0].name, "AEROSOL_ATSR2_SU_L3_V4.2_DAILY")
 
-            data_sources = io.query_data_sources(name="AEROSOL_ATSR2_SU_L3_V4.2_DAILY")
+            data_sources = ds.query_data_sources(name="AEROSOL_ATSR2_SU_L3_V4.2_DAILY")
             self.assertIsNotNone(data_sources)
             self.assertEqual(len(data_sources), 1)
 
-            data_sources = io.query_data_sources(name="ZZ")
+            data_sources = ds.query_data_sources(name="ZZ")
             self.assertIsNotNone(data_sources)
             self.assertEqual(len(data_sources), 0)
         finally:
-            io.DATA_STORE_REGISTRY._data_stores.clear()
+            ds.DATA_STORE_REGISTRY._data_stores.clear()
             for data_store in orig_stores:
-                io.DATA_STORE_REGISTRY.add_data_store(data_store)
-        self.assertEqual(size_before, len(io.DATA_STORE_REGISTRY))
+                ds.DATA_STORE_REGISTRY.add_data_store(data_store)
+        self.assertEqual(size_before, len(ds.DATA_STORE_REGISTRY))
 
     def test_query_data_sources_with_data_store_value(self):
-        data_sources = io.query_data_sources(data_stores=self.TEST_DATA_STORE)
+        data_sources = ds.query_data_sources(data_stores=self.TEST_DATA_STORE)
         self.assertIsNotNone(data_sources)
         self.assertEqual(len(data_sources), 2)
         self.assertEqual(data_sources[0].name, "aerosol")
@@ -109,7 +109,7 @@ class IOTest(TestCase):
 
     def test_query_data_sources_with_data_store_list(self):
         data_stores = [self.TEST_DATA_STORE, self.TEST_DATA_STORE_SST]
-        data_sources = io.query_data_sources(data_stores=data_stores)
+        data_sources = ds.query_data_sources(data_stores=data_stores)
         self.assertIsNotNone(data_sources)
         self.assertEqual(len(data_sources), 3)
         self.assertEqual(data_sources[0].name, "aerosol")
@@ -117,31 +117,31 @@ class IOTest(TestCase):
         self.assertEqual(data_sources[2].name, "sst")
 
     def test_query_data_sources_with_constrains(self):
-        data_sources = io.query_data_sources(data_stores=self.TEST_DATA_STORE, name="aerosol")
+        data_sources = ds.query_data_sources(data_stores=self.TEST_DATA_STORE, name="aerosol")
         self.assertIsNotNone(data_sources)
         self.assertEqual(len(data_sources), 1)
         self.assertEqual(data_sources[0].name, "aerosol")
 
-        data_sources = io.query_data_sources(data_stores=self.TEST_DATA_STORE, name="ozone")
+        data_sources = ds.query_data_sources(data_stores=self.TEST_DATA_STORE, name="ozone")
         self.assertIsNotNone(data_sources)
         self.assertEqual(len(data_sources), 1)
         self.assertEqual(data_sources[0].name, "ozone")
 
-        data_sources = io.query_data_sources(data_stores=self.TEST_DATA_STORE, name="Z")
+        data_sources = ds.query_data_sources(data_stores=self.TEST_DATA_STORE, name="Z")
         self.assertIsNotNone(data_sources)
         self.assertEqual(len(data_sources), 0)
 
     def test_open_dataset(self):
         with self.assertRaises(ValueError) as cm:
-            io.open_dataset(None)
+            ds.open_dataset(None)
         self.assertEqual('No data_source given', str(cm.exception))
 
         with self.assertRaises(ValueError) as cm:
-            io.open_dataset('foo')
+            ds.open_dataset('foo')
         self.assertEqual("No data_source found for the given query term 'foo'", str(cm.exception))
 
         inmem_data_source = InMemoryDataSource(42)
-        dataset1 = io.open_dataset(inmem_data_source)
+        dataset1 = ds.open_dataset(inmem_data_source)
         self.assertIsNotNone(dataset1)
         self.assertIsInstance(dataset1, xr.Dataset)
         self.assertEqual(42, dataset1.a.values)
@@ -155,10 +155,10 @@ class IOTest(TestCase):
             ds_a1 = SimpleDataSource('duplicate')
             ds_a2 = SimpleDataSource('duplicate')
             duplicated_cat = SimpleDataStore('duplicated_cat', [ds_a1, ds_a2])
-            io.DATA_STORE_REGISTRY.add_data_store(duplicated_cat)
+            ds.DATA_STORE_REGISTRY.add_data_store(duplicated_cat)
             with self.assertRaises(ValueError) as cm:
-                io.open_dataset('duplicate')
+                ds.open_dataset('duplicate')
             self.assertEqual("2 data_sources found for the given query term 'duplicate'", str(cm.exception))
         finally:
-            io.DATA_STORE_REGISTRY.remove_data_store('duplicated_cat')
+            ds.DATA_STORE_REGISTRY.remove_data_store('duplicated_cat')
 
