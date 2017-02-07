46a994bf7696634b65d6667ca9f9ff356461d66a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-02-07 16:18:04 +0100

    added build script for conda packages on Windows

---
+++ mk-conda.bat
@@ -0,0 +1,5 @@
+call conda build --user ccitools -c conda-forge -c default recipes\cate-util
+call conda build --user ccitools -c conda-forge -c default recipes\cate-lib
+call conda build --user ccitools -c conda-forge -c default recipes\cate
+
+
