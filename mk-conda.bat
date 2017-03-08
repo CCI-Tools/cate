35ae986755e748d9a101a374569ea1002dd45812
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-03-08 11:02:06 +0100

    unused

--- mk-conda.bat
+++
@@ -1,5 +0,0 @@
-call conda build --user ccitools -c conda-forge -c defaults recipes\cate-util
-call conda build --user ccitools -c conda-forge -c defaults recipes\cate-lib
-call conda build --user ccitools -c conda-forge -c defaults recipes\cate
-
-
--- mk-conda.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-conda build --user ccitools -c conda-forge -c defaults recipes/cate-util
-conda build --user ccitools -c conda-forge -c defaults recipes/cate-lib
-conda build --user ccitools -c conda-forge -c defaults recipes/cate
-
-
