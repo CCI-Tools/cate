b5329447e4b26f9c6a7e0d0521345b94fd8b5704
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-03-09 19:01:09 +0100

    generified min-heap

--- cate/util/minheap.py
+++ cate/util/minheap.py
@@ -20,169 +20,270 @@
 # SOFTWARE.
 
 """
-Fast min-heap implementation using ``numba``.
+Fast binary heap (min-heap) implementation using ``numba``.
 
-See
-
-* https://en.wikipedia.org/wiki/Binary_heap
-* (german version is more detailed: https://de.wikipedia.org/wiki/Bin%C3%A4rer_Heap)
+See https://en.wikipedia.org/wiki/Binary_heap
+(implementation is based on german version at https://de.wikipedia.org/wiki/Bin%C3%A4rer_Heap)
 
 """
 
 __author__ = "Norman Fomferra (Brockmann Consult GmbH)"
 
-import math
+from typing import Union, Tuple, Optional
 
 import numba
+import numpy as np
+
+KeyType = Union[int, float]
+ValueType = Union[bool, int, float]
+KeyArray = np.ndarray
+ValueArray = np.ndarray
 
 
 @numba.jit(nopython=True)
-def build(array):
+def build(keys: KeyArray, values: ValueArray, size: int) -> None:
     """
     Turn the given array into a min-heap.
-
-    :param array: The array to be heapified in place.
-    :return: The heapified array.
     """
-    size = len(array)
+    assert 0 <= size <= keys.size, "size out of bounds"
     if size > 1:
         n = size >> 1
         for i in range(0, n):
             index = n - i - 1
-            _heapify(array, size, index)
-    return array
+            _heapify(keys, values, size, index)
 
 
 @numba.jit(nopython=True)
-def get_min(heap, size):
-    """
-    Get the minimum value.
-
-    :param heap: The heap array.
-    :param size: The heap size, ``1 <= size <= len(heap)``.
-    :return: The minimum value.
-    """
-    assert 1 <= size <= len(heap), "size out of bounds"
-    return heap[0]
-
-
-@numba.jit(nopython=True)
-def remove_min(heap, size):
-    """
-    Remove the minimum value.
-
-    :param heap: The heap array.
-    :param size: The heap size, ``1 <= size <= len(heap)``.
-    :return:
-    """
-    assert 1 <= size <= len(heap), "size out of bounds"
-    return remove(heap, size, 0)
-
-
-@numba.jit(nopython=True)
-def add(heap, size, new_value):
+def add(keys: KeyArray, values: ValueArray, size: int,
+        max_key: KeyType, new_key: KeyType, new_value: ValueType) -> int:
     """
     Add a new value to the heap.
 
-    :param heap: The heap array.
-    :param size: The heap size, ``0 <= size < len(heap)``.
-    :param new_value: The new value to be inserted.
+    :param keys: The heap's keys, ``0 <= size <= keys.size``.
+    :param values: The heap's values, ``0 <= size <= values.size``.
+    :param size: The heap's current size.
+    :param max_key: The maximum key value.
+    :param new_key: The new key to be inserted.
     :return: The new heap size.
     """
-    assert 0 <= size < len(heap), "size out of bounds"
+    assert 0 <= size < keys.size, "size out of bounds"
     index = size
     size += 1
-    heap[index] = math.inf
-    _decrease(heap, size, index, new_value)
+    keys[index] = max_key
+    values[index] = 0
+    _decrease(keys, values, size, index, new_key, new_value)
     return size
 
 
 @numba.jit(nopython=True)
-def remove(heap, size, index):
+def remove(keys: KeyArray, values: ValueArray, size: int,
+           min_key: KeyType, index: int) -> int:
     """
     Remove a value from the heap.
 
-    :param heap: The heap array.
-    :param size: The heap size, ``0 <= size <= len(heap)``.
-    :param index: The index.
+    :param keys: The heap's keys, ``0 <= size <= keys.size``.
+    :param values: The heap's values, ``0 <= size <= values.size``.
+    :param size: The heap's current size.
+    :param min_key: The minimum key value.
+    :param index: The index of the element to be removed.
     :return: The new heap size
     """
-    assert 0 <= size <= len(heap), "size out of bounds"
+    assert 0 <= size <= keys.size, "size out of bounds"
     assert 0 <= index < size, "index out of bounds"
     last_i = size - 1
-    _swap(heap, index, last_i)
     size = last_i
     if index != last_i:
-        if index == 0 or heap[index] > heap[_parent(index)]:
-            _heapify(heap, size, index)
+        _swap(keys, values, index, last_i)
+        # TODO (forman): make sure (test!) that size arg is correct here. Is it the old size (size + 1)?
+        if index == 0 or keys[index] > keys[_parent(index)]:
+            _heapify(keys, values, size, index)
         else:
             # decrease does nothing, if h[i] == h[parent(i)]
-            _decrease(heap, size, index, -math.inf)
+            _decrease(keys, values, size, index, min_key, 0)
     return size
 
 
 @numba.jit(nopython=True)
-def _heapify(heap, size, index):
+def remove_min(keys: KeyArray, values: ValueArray, size: int,
+               min_key: KeyType) -> int:
     """
-    :param heap: The heap array.
-    :param size: The heap size, ``0 <= size <= len(heap)``.
+    Remove the heap's current minimum value (element at index 0).
+
+    :param keys: The heap's keys, ``0 <= size <= keys.size``.
+    :param values: The heap's values, ``0 <= size <= values.size``.
+    :param size: The heap's current size.
+    :param min_key: The minimum key value.
+    :return: The new heap size
+    """
+    return remove(keys, values, size, min_key, 0)
+
+
+@numba.jit(nopython=True)
+def _heapify(keys: KeyArray, values: ValueArray, size: int, index: int) -> None:
+    """
+    :param keys: The heap's keys, ``0 <= size <= keys.size``.
+    :param values: The heap's values, ``0 <= size <= values.size``.
+    :param size: The heap's current size.
     :param index: The index, ``0 <= index < size``.
     """
-    assert 0 <= size <= len(heap), "size out of bounds"
+    assert 0 <= size <= keys.size, "size out of bounds"
     assert 0 <= index < size, "index out of bounds"
     i = index
     while True:
-        # assert isheap(h, left(i)) and isheap(h, right(i))
         min_i = i
         left_i = _left(i)
-        if left_i < size and heap[left_i] < heap[min_i]:
+        if left_i < size and keys[left_i] < keys[min_i]:
             min_i = left_i
         right_i = _right(i)
-        if right_i < size and heap[right_i] < heap[min_i]:
+        if right_i < size and keys[right_i] < keys[min_i]:
             min_i = right_i
         if min_i == i:
             break
-        _swap(heap, i, min_i)
+        _swap(keys, values, i, min_i)
         i = min_i
 
 
 @numba.jit(nopython=True)
-def _decrease(heap, size, index, new_value):
+def _decrease(keys: KeyArray, values: ValueArray, size: int, index: int,
+              new_key: KeyType, new_value: ValueType):
     """
-    :param heap: The heap array.
-    :param size: The heap size, ``1 <= size <= len(heap)``.
+    :param keys: The heap's keys, ``1 <= size <= keys.size``.
+    :param values: The heap's values, ``1 <= size <= values.size``.
+    :param size: The heap's current size.
     :param index: The index, ``0 <= index < size``.
-    :param new_value: The new value, ``heap[index] >= new_value``.
+    :param new_key: The new key, ``keys[index] >= new_key``.
+    :param new_value: Any new value
     """
-    assert 1 <= size <= len(heap), "size out of bounds"
+    assert 1 <= size <= keys.size, "size out of bounds of keys"
+    assert 1 <= size <= values.size, "size out of bounds of values"
     assert 0 <= index < size, "index out of bounds"
-    assert heap[index] >= new_value
-    heap[index] = new_value
+    assert keys[index] >= new_key
+    keys[index] = new_key
+    values[index] = new_value
     while index > 0:
         parent_i = _parent(index)
-        if heap[index] >= heap[parent_i]:
+        if keys[index] >= keys[parent_i]:
             break
-        _swap(heap, index, parent_i)
+        _swap(keys, values, index, parent_i)
         index = parent_i
 
 
 @numba.jit(nopython=True)
-def _parent(i):
-    return (i - 1) >> 1
+def _swap(keys: KeyArray, values: ValueArray, index1: int, index2: int) -> None:
+    key1 = keys[index1]
+    keys[index1] = keys[index2]
+    keys[index2] = key1
+    value1 = values[index1]
+    values[index1] = values[index2]
+    values[index2] = value1
 
 
 @numba.jit(nopython=True)
-def _left(i):
-    return (i << 1) + 1
+def _parent(index: int) -> int:
+    return (index - 1) >> 1
 
 
 @numba.jit(nopython=True)
-def _right(i):
-    return (i << 1) + 2
+def _left(index: int) -> int:
+    return (index << 1) + 1
 
 
 @numba.jit(nopython=True)
-def _swap(h, i, j):
-    t = h[i]
-    h[i] = h[j]
-    h[j] = t
+def _right(index: int) -> int:
+    return (index << 1) + 2
+
+
+class MinHeap:
+    """
+    A min-heap.
+
+    :param keys: Initial key array. The size of this array determines the maximum capacity of the min-heap.
+    :param values: Initial values. This is usually an array of indices into the actual values.
+    :param size: Initial heap size.
+    :param min_key: The smallest possible key value.
+    :param max_key: The largest possible key value.
+    """
+
+    def __init__(self,
+                 keys: KeyArray,
+                 values: Optional[ValueArray] = None,
+                 size: Optional[int] = None,
+                 min_key: Optional[KeyType] = None,
+                 max_key: Optional[KeyType] = None):
+
+        if keys is None:
+            raise ValueError('keys must be given')
+        if keys.size == 0:
+            raise ValueError('keys must not be empty')
+        if values is not None and values.size < keys.size:
+            raise ValueError('values.size must greater than or equal to keys.size')
+        if size is not None and size > keys.size:
+            raise ValueError('size must be less than or equal to keys.size')
+        if min_key is not None and max_key is not None and max_key <= min_key:
+            raise ValueError('min_key must be less than max_key')
+
+        if values is None:
+            values = np.arange(keys.size, dtype=np.uint32)
+        if size is None:
+            size = keys.size
+
+        if min_key is None:
+            dtype = keys.dtype
+            min_key = np.iinfo(dtype).min if np.issubdtype(dtype, np.int) else np.finfo(dtype).min
+        if max_key is None:
+            dtype = keys.dtype
+            max_key = np.iinfo(dtype).max if np.issubdtype(dtype, np.int) else np.finfo(dtype).max
+
+        build(keys, values, size)
+
+        self._keys = keys
+        self._values = values
+        self._size = size
+        self._min_key = min_key
+        self._max_key = max_key
+
+    @property
+    def keys(self) -> KeyArray:
+        return self._keys
+
+    @property
+    def values(self) -> ValueArray:
+        return self._values
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @property
+    def min(self) -> Tuple[KeyType, ValueType]:
+        return self._keys[0], self._values[0]
+
+    @property
+    def min_key(self) -> KeyType:
+        return self._keys[0]
+
+    @property
+    def min_value(self) -> ValueType:
+        return self._values[0]
+
+    def get(self, index: int) -> Tuple[KeyType, ValueType]:
+        return self._keys[index], self._values[index]
+
+    def get_key(self, index: int) -> KeyType:
+        return self._keys[index]
+
+    def get_value(self, index: int) -> ValueType:
+        return self._values[index]
+
+    def add(self, new_key: KeyType, new_value: Optional[ValueType] = None) -> None:
+        if new_value is None:
+            new_value = self._size
+        self._size = add(self._keys, self._values, self._size, self._max_key, new_key, new_value)
+
+    def remove(self, index: int) -> Tuple[KeyType, ValueType]:
+        old_entry = self._keys[index], self._values[index]
+        self._size = remove(self._keys, self._values, self._size, self._min_key, index)
+        return old_entry
+
+    def remove_min(self) -> Tuple[KeyType, ValueType]:
+        return self.remove(0)
--- test/util/test_minheap.py
+++ test/util/test_minheap.py
@@ -1,152 +1,152 @@
 from unittest import TestCase
 
-import numba
+import numpy as np
+from numpy.testing import assert_almost_equal
 
-import cate.util.minheap as minheap
+from cate.util.minheap import MinHeap
 
 
 class MinHeapTest(TestCase):
-    def test_build(self):
-        # self.assertEqual(minheap.build([]), [])
-        self.assertEqual(minheap.build([1]), [1])
-        self.assertEqual(minheap.build([2, 1]), [1, 2])
-        self.assertEqual(minheap.build([3, 2, 1]), [1, 2, 3])
-        self.assertEqual(minheap.build([4, 3, 2, 1]), [1, 3, 2, 4])
-        self.assertEqual(minheap.build([5, 4, 3, 2, 1]), [1, 2, 3, 5, 4])
-        self.assertEqual(minheap.build([6, 5, 4, 3, 2, 1]), [1, 2, 4, 3, 5, 6])
-        self.assertEqual(minheap.build([7, 6, 5, 4, 3, 2, 1]), [1, 3, 2, 4, 6, 7, 5])
+    def test_init_and_properties(self):
+        h = MinHeap(np.zeros(4, dtype=np.float), values=None, size=0)
+        self.assertEqual(h.size, 0)
+        assert_almost_equal(h.keys, [0., 0., 0., 0.])
+        assert_almost_equal(h.values, [0, 1, 2, 3])
 
-    def test_get_min(self):
-        self.assertEqual(minheap.get_min([1.1, 1.2, 1.3], 3), 1.1)
+        h = MinHeap(np.array([1], dtype=np.float))
+        self.assertEqual(h.size, 1)
+        assert_almost_equal(h.keys, [1.])
+        assert_almost_equal(h.values, [0])
+
+        h = MinHeap(np.array([2, 1], dtype=np.float))
+        self.assertEqual(h.size, 2)
+        assert_almost_equal(h.keys, [1., 2.])
+        assert_almost_equal(h.values, [1, 0])
+
+        h = MinHeap(np.array([3, 2, 1], dtype=np.float))
+        self.assertEqual(h.size, 3)
+        assert_almost_equal(h.keys, [1., 2., 3.])
+        assert_almost_equal(h.values, [2, 1, 0])
+
+        h = MinHeap(np.array([4, 3, 2, 1], dtype=np.float))
+        self.assertEqual(h.size, 4)
+        assert_almost_equal(h.keys, [1., 3., 2., 4.])
+        assert_almost_equal(h.values, [3, 1, 2, 0])
+
+        h = MinHeap(np.array([5, 4, 3, 2, 1], dtype=np.float))
+        self.assertEqual(h.size, 5)
+        assert_almost_equal(h.keys, [1., 2., 3., 5., 4.])
+        assert_almost_equal(h.values, [4, 3, 2, 0, 1])
+
+        h = MinHeap(np.array([6, 5, 4, 3, 2, 1], dtype=np.float))
+        self.assertEqual(h.size, 6)
+        assert_almost_equal(h.keys, [1., 2., 4., 3., 5., 6.])
+        assert_almost_equal(h.values, [5, 4, 2, 3, 1, 0])
+
+        h = MinHeap(np.array([7, 6, 5, 4, 3, 2, 1], dtype=np.float))
+        self.assertEqual(h.size, 7)
+        assert_almost_equal(h.keys, [1., 3., 2., 4., 6., 7., 5.])
+        assert_almost_equal(h.values, [6, 4, 5, 3, 1, 0, 2])
+
+    def test_min(self):
+        h = MinHeap(np.array([1.2, 1.3, 1.1]), values=np.array([10, 25, 19]))
+        self.assertAlmostEqual(h.min[0], 1.1)
+        self.assertEqual(h.min[1], 19)
+        self.assertEqual(h.min[0], h.min_key)
+        self.assertEqual(h.min[1], h.min_value)
+
+    def test_get(self):
+        h = MinHeap(np.array([1.2, 1.3, 1.1]), values=np.array([10, 25, 19]))
+        self.assertAlmostEqual(h.get(2)[0], 1.2)
+        self.assertEqual(h.get(2)[1], 10)
+        self.assertEqual(h.get(2)[0], h.get_key(2))
+        self.assertEqual(h.get(2)[1], h.get_value(2))
 
     def test_add(self):
-        heap = [0.] * 10
-        size = 0
+        h = MinHeap(np.empty(6, dtype=np.float), values=None, size=0)
 
-        size = minheap.add(heap, size, 4.)
-        self.assertEqual(size, 1)
-        self.assertEqual(heap[:size], [4.])
+        h.add(4., 0)
+        self.assertEqual(h.size, 1)
+        assert_almost_equal(h.keys, [4., 0., 0., 0., 0., 0.])
+        assert_almost_equal(h.values, [0, 1, 2, 3, 4, 5])
 
-        size = minheap.add(heap, size, 2.)
-        self.assertEqual(size, 2)
-        self.assertEqual(heap[:size], [2, 4])
+        h.add(2., 1)
+        self.assertEqual(h.size, 2)
+        assert_almost_equal(h.keys, [2., 4., 0., 0., 0., 0.])
+        assert_almost_equal(h.values, [1, 0, 2, 3, 4, 5])
 
-        size = minheap.add(heap, size, 6.)
-        self.assertEqual(size, 3)
-        self.assertEqual(heap[:size], [2, 4, 6])
+        h.add(6.)
+        self.assertEqual(h.size, 3)
+        assert_almost_equal(h.keys, [2., 4., 6., 0., 0., 0.])
+        assert_almost_equal(h.values, [1, 0, 2, 3, 4, 5])
 
-        size = minheap.add(heap, size, 5.)
-        self.assertEqual(size, 4)
-        self.assertEqual(heap[:size], [2., 4., 6., 5.])
+        h.add(5.)
+        self.assertEqual(h.size, 4)
+        assert_almost_equal(h.keys, [2., 4., 6., 5., 0., 0.])
+        assert_almost_equal(h.values, [1, 0, 2, 3, 4, 5])
 
-        size = minheap.add(heap, size, 1.)
-        self.assertEqual(size, 5)
-        self.assertEqual(heap[:size], [1., 2., 6., 5., 4.])
+        h.add(1., 4)
+        self.assertEqual(h.size, 5)
+        assert_almost_equal(h.keys, [1., 2., 6., 5., 4., 0.])
+        assert_almost_equal(h.values, [4, 1, 2, 3, 0, 5])
 
-        size = minheap.add(heap, size, 3.)
-        self.assertEqual(size, 6)
-        self.assertEqual(heap[:size], [1., 2., 3., 5., 4., 6.])
+        h.add(3., 67)
+        self.assertEqual(h.size, 6)
+        assert_almost_equal(h.keys, [1., 2., 3., 5., 4., 6.])
+        assert_almost_equal(h.values, [4, 1, 67, 3, 0, 2])
 
     def test_remove(self):
-        heap = [1., 2., 4., 3., 5., 6., 0., 0., 0., 0.]
-        size = 6
+        h = MinHeap(np.array([1., 2., 4., 3., 5., 6.]))
+        self.assertEqual(h.size, 6)
+        assert_almost_equal(h.keys, [1., 2., 4., 3., 5., 6.])
+        assert_almost_equal(h.values, [0, 1, 2, 3, 4, 5])
 
-        size = minheap.remove(heap, size, 4)
-        self.assertEqual(size, 5)
-        self.assertEqual(heap[:size], [1., 2., 4., 3., 6.])
+        removed_entry = h.remove(4)
+        self.assertEqual(removed_entry, (5., 4))
+        self.assertEqual(h.size, 5)
+        assert_almost_equal(h.keys, [1.,  2.,  4.,  3.,  6.,  5.])
+        assert_almost_equal(h.values, [0, 1, 2, 3, 5, 4])
 
-        size = minheap.remove(heap, size, 3)
-        self.assertEqual(size, 4)
-        self.assertEqual(heap[:size], [1., 2., 4., 6.])
+        removed_entry = h.remove(3)
+        self.assertEqual(removed_entry, (3., 3))
+        self.assertEqual(h.size, 4)
+        assert_almost_equal(h.keys, [1.,  2.,  4.,  6.,  3.,  5.])
+        assert_almost_equal(h.values, [0, 1, 2, 5, 3, 4])
 
-        size = minheap.remove(heap, size, 0)
-        self.assertEqual(size, 3)
-        self.assertEqual(heap[:size], [2., 6., 4.])
+        removed_entry = h.remove(0)
+        self.assertEqual(removed_entry, (1., 0))
+        self.assertEqual(h.size, 3)
+        assert_almost_equal(h.keys, [2.,  6.,  4.,  1.,  3.,  5.])
+        assert_almost_equal(h.values, [1, 5, 2, 0, 3, 4])
 
-        size = minheap.remove(heap, size, 2)
-        self.assertEqual(size, 2)
-        self.assertEqual(heap[:size], [2., 6.])
+        removed_entry = h.remove(2)
+        self.assertEqual(removed_entry, (4., 2))
+        self.assertEqual(h.size, 2)
+        assert_almost_equal(h.keys, [2.,  6.,  4.,  1.,  3.,  5.])
+        assert_almost_equal(h.values, [1, 5, 2, 0, 3, 4])
 
-        size = minheap.remove(heap, size, 1)
-        self.assertEqual(size, 1)
-        self.assertEqual(heap[:size], [2.])
+        removed_entry = h.remove(1)
+        self.assertEqual(removed_entry, (6.0, 5))
+        self.assertEqual(h.size, 1)
+        assert_almost_equal(h.keys, [2.,  6.,  4.,  1.,  3.,  5.])
+        assert_almost_equal(h.values, [1, 5, 2, 0, 3, 4])
 
-        size = minheap.remove(heap, size, 0)
-        self.assertEqual(size, 0)
-        self.assertEqual(heap[:size], [])
+        removed_entry = h.remove(0)
+        self.assertEqual(removed_entry, (2., 1))
+        self.assertEqual(h.size, 0)
+        assert_almost_equal(h.keys, [2.,  6.,  4.,  1.,  3.,  5.])
+        assert_almost_equal(h.values, [1, 5, 2, 0, 3, 4])
 
     def test_remove_min(self):
-        heap = [1., 2., 4., 3., 5., 6., 7.]
-        size = 7
+        h = MinHeap(np.array([1., 2., 4., 3., 5., 6., 7.]))
+        self.assertEqual(h.size, 7)
+        assert_almost_equal(h.keys, [1., 2., 4., 3., 5., 6., 7.])
+        assert_almost_equal(h.values, [0, 1, 2, 3, 4, 5, 6])
 
-        size = minheap.remove_min(heap, size)
-        self.assertEqual(size, 6)
-        self.assertEqual(heap[:size], [2., 3., 4., 7., 5., 6.])
+        removed_entry = h.remove_min()
+        self.assertEqual(removed_entry, (1., 0))
+        self.assertEqual(h.size, 6)
+        assert_almost_equal(h.keys, [2.,  3.,  4.,  7.,  5.,  6.,  1.])
+        assert_almost_equal(h.values, [1, 3, 2, 6, 4, 5, 0])
 
-        size = minheap.remove_min(heap, size)
-        self.assertEqual(size, 5)
-        self.assertEqual(heap[:size], [3., 5., 4., 7., 6.])
 
-        size = minheap.remove_min(heap, size)
-        self.assertEqual(size, 4)
-        self.assertEqual(heap[:size], [4., 5., 6., 7.])
-
-        size = minheap.remove_min(heap, size)
-        self.assertEqual(size, 3)
-        self.assertEqual(heap[:size], [5., 7., 6.])
-
-        size = minheap.remove_min(heap, size)
-        self.assertEqual(size, 2)
-        self.assertEqual(heap[:size], [6., 7.])
-
-        size = minheap.remove_min(heap, size)
-        self.assertEqual(size, 1)
-        self.assertEqual(heap[:size], [7.])
-
-    def test_can_pass_numba_function(self):
-        @numba.jit(nopython=True)
-        def g(x):
-            return 3 * x
-
-        def f(g, a):
-            return g(a)
-
-        self.assertAlmostEqual(f(g, 1.4), 4.2)
-
-    def test_can_use_numba_function_in_numba_function(self):
-        @numba.jit(nopython=True)
-        def g(x: float) -> float:
-            return 3. * x
-
-        @numba.jit(nopython=True)
-        def f(a):
-            return g(a)
-
-        self.assertAlmostEqual(f(1.4), 4.2)
-
-# import numba
-# import numpy as np
-#
-# @numba.jit(nopython=True)
-# def area(x1: float, y1: float, x2: float, y2: float) -> float:
-#     return 0.5 * abs(x1 * y2 - y1 * x2)
-#
-# @numba.jit(nopython=True)
-# def simplify(n, x, y):
-#     if n < 4:
-#         return
-#     m = n - 2
-#     areas = np.empty(m)
-#     min_area = 1e10
-#     min_area_i = -1
-#     for i in range(m):
-#         x0 = x[i]
-#         y0 = y[i]
-#         a = area(x[i + 1] - x0, y[i + 1] - y0, x[i + 2] - x0, y[i + 2] - y0)
-#         if a < min_area:
-#             min_area = a
-#             min_area_i = i
-#         areas[i] = a
-#
-#     if min_area_i >= 0:
-#         pass
