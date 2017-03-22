0e917d1291326fbf1f1dc3e26fdf4f7dd18f6830
Janis Gailis <gailis@stcorp.no>
2017-03-22 17:17:26 +0100

    Add a test to see if it works with Dask

--- test/ops/test_anomaly.py
+++ test/ops/test_anomaly.py
@@ -26,7 +26,8 @@ class TestExternal(TestCase):
     Test anomaly calculation with external reference
     """
     def setUp(self):
-        self._TEMP = 'temp.nc'
+        self._TEMP = 'temp_ref.nc'
+        self._TEMP_DS = 'temp_ds.nc'
 
     def tearDown(self):
         self.cleanup()
@@ -34,6 +35,11 @@ class TestExternal(TestCase):
     def cleanup(self):
         try:
             os.remove(self._TEMP)
+        except:
+            pass
+
+        try:
+            os.remove(self._TEMP_DS)
         except:
             pass
 
@@ -105,6 +111,44 @@ class TestExternal(TestCase):
                                           self._TEMP,
                                           transform='log10, +3')
         assert_dataset_equal(actual, expected)
+
+    def test_dask(self):
+        # Test if the operation works with xarray datasets with dask as the
+        # backend.
+        pass
+        ref = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.ones([45, 90, 12])),
+            'second': (['lat', 'lon', 'time'], np.ones([45, 90, 12])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90)})
+
+        ds = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.ones([45, 90, 24])),
+            'second': (['lat', 'lon', 'time'], np.ones([45, 90, 24])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90),
+            'time': [datetime(2000, x, 1) for x in range(1,13)]+\
+                    [datetime(2001, x, 1) for x in range(1,13)]})
+
+        expected = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.zeros([45, 90, 24])),
+            'second': (['lat', 'lon', 'time'], np.zeros([45, 90, 24])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90),
+            'time': [datetime(2000, x, 1) for x in range(1,13)]+\
+                    [datetime(2001, x, 1) for x in range(1,13)]})
+
+        # Test that ds is not a dask array
+        self.assertTrue(not ds.chunks)
+        ref.to_netcdf(self._TEMP)
+        ds.to_netcdf(self._TEMP_DS)
+        # This makes ds a dask dataset in xarray backend
+        ds = xr.open_dataset(self._TEMP_DS, chunks={})
+
+        actual = anomaly.anomaly_external(ds, self._TEMP)
+        assert_dataset_equal(actual, expected)
+        # Test that actual is also a dask array, based on ds
+        self.assertEqual(actual.chunks, ds.chunks)
 
     def test_registered(self):
         reg_op = OP_REGISTRY.get_op(object_to_qualified_name(anomaly.anomaly_external))
