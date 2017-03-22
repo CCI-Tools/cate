ffa265e412fea501f42cc9a61b3df6649951e77f
Janis Gailis <gailis@stcorp.no>
2017-03-22 14:02:41 +0100

    WIP

--- test/ops/test_anomaly.py
+++ test/ops/test_anomaly.py
@@ -9,3 +9,15 @@ import xarray as xr
 import datetime as datetime
 
 from cate.ops import anomaly
+from cate.core.op import OP_REGISTRY
+from cate.util.misc import object_to_qualified_name
+
+
+def assert_dataset_equal(expected, actual):
+    # this method is functionally equivalent to
+    # `assert expected == actual`, but it checks each aspect
+    # of equality separately for easier debugging
+    assert expected.equals(actual), (expected, actual)
+
+
+class Test
