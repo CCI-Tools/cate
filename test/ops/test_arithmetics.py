dd87c5dd0c0925aa871cde591c7f64efa143cd64
Janis Gailis <gailis@stcorp.no>
2017-03-22 13:17:26 +0100

    Difference operation and tests
    A more robust difference operation has been implemented along with
    appropriate tests.

--- test/ops/test_arithmetics.py
+++ test/ops/test_arithmetics.py
@@ -6,6 +6,7 @@ from unittest import TestCase
 
 import numpy as np
 import xarray as xr
+from datetime import datetime
 
 from cate.ops import arithmetics
 from cate.core.op import OP_REGISTRY
@@ -54,7 +55,106 @@ class TestDsArithmetics(TestCase):
 
 class TestDiff(TestCase):
     """
-    Test the difference between two datasets
+    Test taking the difference between two datasets
     """
     def test_diff(self):
-        pass
+        # Test nominal
+        dataset = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
+            'second': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90)})
+
+        expected = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
+            'second': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90)})
+        actual = arithmetics.diff(dataset, dataset*2)
+        assert_dataset_equal(expected*-1, actual)
+
+        # Test variable mismatch
+        ds = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
+            'third': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90)})
+
+        ds1 = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
+            'second': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90)})
+
+        expected = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.zeros([45, 90, 3])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90)})
+
+        actual = arithmetics.diff(ds, ds1)
+        assert_dataset_equal(expected, actual)
+        actual = arithmetics.diff(ds1, ds)
+        assert_dataset_equal(expected, actual)
+
+        # Test date range mismatch
+        ds = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.ones([45, 90, 12])),
+            'second': (['lat', 'lon', 'time'], np.ones([45, 90, 12])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90),
+            'time': [datetime(2000, x, 1) for x in range(1, 13)]})
+
+        ds1 = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.ones([45, 90, 12])),
+            'second': (['lat', 'lon', 'time'], np.ones([45, 90, 12])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90),
+            'time': [datetime(2003, x, 1) for x in range(1, 13)]})
+
+        expected = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.zeros([45, 90, 12])),
+            'second': (['lat', 'lon', 'time'], np.zeros([45, 90, 12])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90)})
+
+        actual = arithmetics.diff(ds, ds1)
+        assert_dataset_equal(actual, expected)
+
+        actual = arithmetics.diff(ds, ds1.drop('time'))
+        expected['time'] = [datetime(2000, x, 1) for x in range(1,13)]
+        assert_dataset_equal(actual, expected)
+
+        # Test broadcasting
+        ds = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
+            'second': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90)})
+
+        ds1 = xr.Dataset({
+            'first': (['lat', 'lon'], np.ones([45, 90])),
+            'second': (['lat', 'lon'], np.ones([45, 90])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90)})
+
+        expected = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.zeros([45, 90, 3])),
+            'second': (['lat', 'lon', 'time'], np.zeros([45, 90, 3])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90)})
+        actual = arithmetics.diff(ds, ds1)
+        assert_dataset_equal(expected, actual)
+
+        ds['time'] = [datetime(2000, x, 1) for x in range(1,4)]
+        expected['time'] = [datetime(2000, x, 1) for x in range(1,4)]
+        actual = arithmetics.diff(ds, ds1)
+        assert_dataset_equal(expected, actual)
+
+        ds1 = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.ones([45, 90, 1])),
+            'second': (['lat', 'lon', 'time'], np.ones([45, 90, 1])),
+            'lat': np.linspace(-88, 88, 45),
+            'lon': np.linspace(-178, 178, 90),
+            'time': [datetime(2001,1,1)]})
+        actual = arithmetics.diff(ds, ds1)
+        assert_dataset_equal(expected, actual)
