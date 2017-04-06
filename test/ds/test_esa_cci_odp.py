3c2eca1ef7ebe7a59656a3e246f713092ca890ce
Krzysztof (Chris) Bernat <chris.bernat@telespazio.com>
2017-04-06 14:25:12 +0100

    #153 test fix, fixed mocked http connection

--- test/ds/test_esa_cci_odp.py
+++ test/ds/test_esa_cci_odp.py
@@ -70,7 +70,7 @@ class EsaCciOdpDataSourceTest(unittest.TestCase):
             def build_file_item(file_name: str, date_from: datetime, date_to: datetime, size: int):
                 return [file_name, date_from, date_to, size,
                         {'OPENDAP': ''+os.path.join(reference_path, file_name),
-                         'HTTPServer': 'file:'+os.path.join(reference_path, file_name)}]
+                         'HTTPServer': 'file://'+os.path.join(reference_path, file_name)}]
 
             reference_files = {
                 'ESACCI-SOILMOISTURE-L3S-SSMV-COMBINED-19781114000000-fv02.2.nc': {
