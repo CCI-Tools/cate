a9d5854d38efd821927e1c2cc9b6ddcd7ad9b46e
Janis Gailis <gailis@stcorp.no>
2017-04-10 14:37:30 +0200

    IO with new types and new ds approach
    Test have been adjusted accordingly but seem to fail localy.

--- test/ops/test_io.py
+++ test/ops/test_io.py
@@ -15,12 +15,13 @@ class TestIO(TestCase):
     @unittest.skip(reason="skipped unless you want to debug data source access")
     def test_open_dataset(self):
         # Test normal functionality
-        dataset = open_dataset('AEROSOL_AATSR_SU_L3_V4.21_MONTHLY', '2008-01-01', '2008-03-01')
+        dataset = open_dataset('AEROSOL_AATSR_SU_L3_V4.21_MONTHLY',
+                               '2008-01-01, 2008-03-01')
         self.assertIsNotNone(dataset)
 
         # Test swapped dates
         with self.assertRaises(ValueError):
-            open_dataset('AEROSOL_AATSR_SU_L3_V4.21_MONTHLY', '2008-03-01', '2008-01-01')
+            open_dataset('AEROSOL_AATSR_SU_L3_V4.21_MONTHLY', '2008-03-01, 2008-01-01')
 
         # Test required arguments
         with self.assertRaises(TypeError):
@@ -29,7 +30,8 @@ class TestIO(TestCase):
     @unittest.skip(reason="skipped unless you want to debug data source access")
     def test_save_dataset(self):
         # Test normal functionality
-        dataset = open_dataset('AEROSOL_AATSR_SU_L3_V4.21_MONTHLY', '2008-01-01', '2008-03-01')
+        dataset = open_dataset('AEROSOL_AATSR_SU_L3_V4.21_MONTHLY',
+                               '2008-01-01, 2008-03-01')
         save_dataset(dataset, 'remove_me.nc')
         self.assertTrue(os.path.isfile('remove_me.nc'))
         os.remove('remove_me.nc')
