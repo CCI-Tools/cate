5b52e7387eabd1f0c24a6a1e3083e24ce9314a09
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2017-04-13 10:27:17 +0200

    add github commit API integration for flake8 tests

---
+++ ci/github_commit_api.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+STATUS="$1"
+TEXT="$2"
+
+REPO="${TRAVIS_REPO_SLUG}"
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+    SHA="${TRAVIS_COMMIT}"
+    CONTEXT="flake8/push"
+else
+    SHA="${TRAVIS_PULL_REQUEST_SHA}"
+    CONTEXT="flake8/pr"
+fi
+LOG_URL="https://travis-ci.org/${TRAVIS_REPO_SLUG}/jobs/${TRAVIS_JOB_ID}"
+
+
+curl "https://api.github.com/repos/${REPO}/statuses/${SHA}?access_token=${GITHUB_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"state\": \"${STATUS}\", \"context\": \"${CONTEXT}\", \"description\": \"${TEXT}\", \"target_url\": \"${LOG_URL}\"}"
