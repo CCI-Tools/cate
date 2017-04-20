dd47be24f7c7b02b5bdcc6e215b8bfb2f8517f77
Krzysztof (Chris) Bernat <chris.bernat@telespazio.com>
2017-04-20 19:23:15 +0100

    #196 fixed spatial coverage caluclation for coordinates data in descending order (e.g. lat: 90 to -90) - Part 2, local data source

--- cate/ds/esa_cci_odp.py
+++ cate/ds/esa_cci_odp.py
@@ -18,7 +18,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 __author__ = "Norman Fomferra (Brockmann Consult GmbH), " \
              "Marco Zühlke (Brockmann Consult GmbH), " \
              "Chris Bernat (Telespazio VEGA UK Ltd)"
