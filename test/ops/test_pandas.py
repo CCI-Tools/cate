6031ce13ddb46eaa4ec95e06fea742027606770c
Janis Gailis <gailis@stcorp.no>
2017-04-24 13:26:11 +0200

    Test skeletons

---
+++ test/ops/test_pandas.py
@@ -0,0 +1,22 @@
+"""
+Tests for pandas wrappers
+"""
+
+from unittest import TestCase
+
+
+class TestFillna(TestCase):
+    """
+    Test fillna operation
+    """
+    def test_nominal(self):
+        """
+        Test nominal operation
+        """
+        pass
+
+    def test_registered(self):
+        """
+        Test operation when run as a registered operation
+        """
+        pass
