95f6a5faa9379c99ce64661a3642791a25ae3f22
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-05-15 09:42:29 +0200

    v0.8.0rc2

--- setup-util.py
+++ setup-util.py
@@ -36,9 +36,10 @@ setup(
     author='Cate Development Team',
     packages=['cate', 'cate.util', 'cate.util.im', 'cate.util.web'],
     install_requires=[
-        'matplotlib >= 1.5,<2',
         'numpy >= 1.7',
-        'tornado >= 4.4',
-        'pillow >= 4.0'
+        # Optional dependencies
+        # 'matplotlib >= 1.5,<2',
+        # 'tornado >= 4.4',
+        # 'pillow >= 4.0'
     ],
 )
