6d33e35685698fa9a73f664e1482ba0571ddb6a1
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-05-22 13:25:29 +0200

    made it run again, but failed, see #241

--- scripts/uc09.bat
+++ scripts/uc09.bat
@@ -1,4 +1,4 @@
-call activate cate
+@echo off
 
 rem Download some CCI Cloud data
 cate ds copy esacci.CLOUD.mon.L3C.CLD_PRODUCTS.MODIS.Terra.MODIS_TERRA.1-0.r1 --name CLOUD_2007 --time 2007,2008
@@ -17,13 +17,18 @@ rem Open the Cloud and Ozone datasets and assign it to resources named "cloud" a
 cate res open cloud local.CLOUD_2007
 cate res open ozone local.OZONE_2007
 
-rem Create subsets of the "cloud" and "ozone" resources and assign it
+rem Coregister "ozone" with "cloud" and call the result "ozone_coreg"
+rem: ERROR HERE, see issue #241
+cate res set ozone_coreg coregister ds_master=@cloud ds_slave=@ozone
+
+rem Create subsets of the "cloud" and "ozone_coreg" resources and assign it
 rem to new resources named "cloud_sub" and "ozone_sub"
 cate res set cloud_sub subset_spatial ds=@cloud region=0,30,10,40
-cate res set ozone_sub subset_spatial ds=@ozone region=0,30,10,40
+cate res set ozone_sub subset_spatial ds=@ozone_coreg region=0,30,10,40
 
-rem Coregister "cloud_sub" with "ozone_sub" and call the result "cloud_res"
-cate res set cloud_res coregister ds_master=@ozone_sub ds_slave=@cloud_sub
+cate res set corr pearson_correlation ds_x=@ozone_sub ds_y=@cloud_sub_resamp var_x=O3_du_tot var_y=cc_total file=corr.txt
+
+cate res print corr
 
 rem Save the workspace
 cate ws save
