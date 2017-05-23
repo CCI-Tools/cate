eedb4dc0469fc2f01d54adcf0770f9de32390196
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-05-23 18:14:38 +0200

    removed activation

--- test-all.bat
+++ test-all.bat
@@ -1,7 +1,5 @@
-call activate cate
+rem Don't forget to activate environment
 set CATE_DISABLE_WEB_TESTS=1
 set CATE_DISABLE_PLOT_TESTS=1
 set CATE_DISABLE_GEOPANDAS_TESTS=1
 py.test test
-rem py.test test\core\test_workspace.py
-rem py.test test/cli/test_cli.py
