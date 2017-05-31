6b94c9f835ca9f6a5c6a3ad9a82c1595da696db7
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-05-31 18:20:59 +0200

    Python 3.6 compatibility

--- cate/ops/plot.py
+++ cate/ops/plot.py
@@ -59,7 +59,12 @@ import xarray as xr
 import pandas as pd
 from matplotlib.figure import Figure
 
-matplotlib.use('Qt4Agg')
+# noinspection PyBroadException
+try:
+    matplotlib.use('Qt5Agg')
+except:
+    matplotlib.use('Qt4Agg')
+
 import matplotlib.pyplot as plt
 
 import cartopy.crs as ccrs
