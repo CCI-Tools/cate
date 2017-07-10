5b3c6fb94c18e72b0dda10b2ad84082d77a1d27e
Janis Gailis <gailis@stcorp.no>
2017-07-10 10:58:14 +0200

    Refactor correlation test
    Test stubs for envisioned correlation tests have been added, existing
    tests refactored into the new scheme and updated to be more sound.

--- test/ops/test_correlation.py
+++ test/ops/test_correlation.py
@@ -11,13 +11,11 @@ from scipy.stats import pearsonr
 from cate.ops.correlation import pearson_correlation_simple, pearson_correlation_map
 
 
-class TestCorrelation(TestCase):
-    def test_pearson(self):
+class TestPearsonSimple(TestCase):
+    def test_nominal(self):
         """
-        General functionality test
+        Test nominal run
         """
-        # Test general functionality 1D dataset variables, test for correctness
-        # of correlation
         dataset = xr.Dataset({
             'first': ('time', np.linspace(0, 5, 6))})
         dataset2 = xr.Dataset({
@@ -31,35 +29,82 @@ class TestCorrelation(TestCase):
         corr_coef = correlation['corr_coef']
         self.assertTrue(np.isclose(corr_coef, 0.755928))
 
+    def test_registered(self):
+        """
+        Nominal run using the operation through OP_REGISTRY
+        """
+        pass
+
+    def test_polymorphism(self):
+        """
+        Nominal run with an xr.Dataset and a pd.DataFrame
+        """
+        pass
+
+    def test_2d(self):
+        """
+        Nominal run with a 2d object
+        """
+        pass
+
+    def test_3d(self):
+        """
+        Nominal run with a 3d object
+        """
+        pass
+
+    def test_error(self):
+        """
+        Test error conditions
+        """
+        pass
+
+
+class TestPearsonMap(TestCase):
+    def test_nominal(self):
+        """
+        Test nominal run
+        """
         # Test general functionality 3D dataset variables
         ds1 = xr.Dataset({
-            'first': (['time', 'lat', 'lon'], np.array([np.eye(4, 8),
-                                                        np.eye(4, 8),
-                                                        np.eye(4, 8)])),
-            'second': (['time', 'lat', 'lon'], np.array([np.eye(4, 8),
-                                                         np.eye(4, 8),
-                                                         np.eye(4, 8)])),
+            'first': (['time', 'lat', 'lon'], np.array([np.ones([4, 8]),
+                                                        np.ones([4, 8]) * 2,
+                                                        np.ones([4, 8]) * 3])),
+            'second': (['time', 'lat', 'lon'], np.array([np.ones([4, 8]) * 2,
+                                                         np.ones([4, 8]) * 3,
+                                                         np.ones([4, 8])])),
             'lat': np.linspace(-67.5, 67.5, 4),
             'lon': np.linspace(-157.5, 157.5, 8),
             'time': np.array([1, 2, 3])})
 
         ds2 = xr.Dataset({
-            'first': (['time', 'lat', 'lon'], np.array([np.eye(4, 8),
-                                                        np.eye(4, 8),
-                                                        np.eye(4, 8)])),
-            'second': (['time', 'lat', 'lon'], np.array([np.eye(4, 8),
-                                                         np.eye(4, 8),
-                                                         np.eye(4, 8)])),
+            'second': (['time', 'lat', 'lon'], np.array([np.ones([4, 8]),
+                                                         np.ones([4, 8]) * 2,
+                                                         np.ones([4, 8]) * 3])),
+            'first': (['time', 'lat', 'lon'], np.array([np.ones([4, 8]) * 2,
+                                                        np.ones([4, 8]) * 3,
+                                                        np.ones([4, 8])])),
             'lat': np.linspace(-67.5, 67.5, 4),
             'lon': np.linspace(-157.5, 157.5, 8),
             'time': np.array([1, 2, 3])})
 
-        correlation = pearson_correlation_map(ds1, ds2, 'first', 'first')
+        corr = pearson_correlation_map(ds1, ds2, 'first', 'first')
+
+        self.assertTrue(corr['corr_coef'].max() == corr['corr_coef'].min())
+        self.assertTrue(corr['corr_coef'].max() == -0.5)
+
+        self.assertTrue(corr['p_value'].max() == corr['p_value'].min())
+        self.assertTrue(np.isclose(corr['p_value'].min(), 0.6666666))
+
+    def test_registered(self):
+        """
+        Nominal run using the operation through OP_REGISTRY
+        """
+        pass
 
     def test_validate_against_scipy(self):
         """
-        Test if results in a 3D array correspond to what would be expected
-        if scipy.stats.pearsonr was used
+        Validate the result against scipy implementation
         """
         x = np.linspace(0, 5, 6)
         y = np.linspace(0, 5, 6)
@@ -89,3 +134,21 @@ class TestCorrelation(TestCase):
                                           cc_sp)))
         self.assertTrue(np.all(np.isclose(correlation['p_value'].values,
                                           pv_sp)))
+
+    def test_broadcasting(self):
+        """
+        Test a (3d, 1d) input pair
+        """
+        pass
+
+    def test_polymorphism(self):
+        """
+        Broadcasting run where the 1d input is a pandas dataframe
+        """
+        pass
+
+    def test_error(self):
+        """
+        Test error conditions
+        """
+        pass
