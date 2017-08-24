17e17ea49bf11f3fabe04dd2c37340ebe0adae80
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2017-08-24 16:56:31 +0200

    typo

--- cate/core/ds.py
+++ cate/core/ds.py
@@ -567,7 +567,7 @@ def open_xarray_dataset(paths, concat_dim='time', **kwargs) -> xr.Dataset:
     #
     # netCDF files can also feature a significant level of compression rendering
     # the known file size on disk useless to determine if the default dask chunk
-    # will be small enough that a few of them ccould comfortably fit in memory for
+    # will be small enough that a few of them could comfortably fit in memory for
     # parallel processing.
     #
     # Hence we open the first file of the dataset, find out its uncompressed size
