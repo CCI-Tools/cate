4ef39c91847bd6141cc3aaeb64dd44e4e59f95a0
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2018-01-15 10:05:23 +0100

    add shapely.errors to mocked imports

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,7 +29,7 @@ import mock
 MOCK_MODULES = ['geopandas', 'cartopy', 'cartopy.crs', 'fiona', 'numba', 'pandas',
                 'matplotlib', 'matplotlib.cm', 'matplotlib.figure', 'matplotlib.pyplot', 'matplotlib.backends.backend_webagg_core',
                 'pyproj', 'scipy', 'scipy.stats', 'scipy.special',
-                'shapely', 'shapely.wkt', 'shapely.geometry', 'shapely.geometry.base', 'xarray', 'xarray.backends',
+                'shapely', 'shapely.errors', 'shapely.wkt', 'shapely.geometry', 'shapely.geometry.base', 'xarray', 'xarray.backends',
                 'dask', 'dask.callbacks',
                 'numpy', 'jdcal', 'dateutil', 'owslib', 'owslib.csw', 'owslib.namespaces', 'psutil']
 for mod_name in MOCK_MODULES:
