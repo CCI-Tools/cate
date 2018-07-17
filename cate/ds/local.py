318db80c0de20ca1ffb8094f19ddfe805f90efd2
papesci <papesci73@yahoo.com>
2018-07-17 10:04:44 +0100

    new line and redundant check removed

--- cate/ds/local.py
+++ cate/ds/local.py
@@ -150,9 +150,9 @@ class LocalDataSource(DataSource):
                      var_names: VarNamesLike.TYPE = None,
                      protocol: str = None,
                      monitor: Monitor = Monitor.NONE) -> Any:
-        time_range = TimeRangeLike.convert(time_range) if time_range else None
-        region = PolygonLike.convert(region) if region else None
-        var_names = VarNamesLike.convert(var_names) if var_names else None
+        time_range = TimeRangeLike.convert(time_range)
+        region = PolygonLike.convert(region)
+        var_names = VarNamesLike.convert(var_names)
 
         paths = []
         if time_range:
