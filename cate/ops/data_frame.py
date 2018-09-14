fb36208cb72f4445d4309d8459266a463b38cb88
Helge <helge.dzierzon@brockmann-consult.de>
2018-09-14 16:01:48 +0200

    Op returns always pd.DataFrame when no geometry is conserved.

--- cate/ops/data_frame.py
+++ cate/ops/data_frame.py
@@ -356,8 +356,6 @@ def data_frame_aggregate(df: DataFrameLike.TYPE,
 
         df_agg = gpd.GeoDataFrame(df_agg, geometry=[multi_polygon])
         df_agg.crs = df.crs
-    elif df_is_geo:
-        df_agg = gpd.GeoDataFrame(df_agg)
 
     return df_agg
 
