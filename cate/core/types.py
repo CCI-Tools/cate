93224635a00b3f9bfaa851c3c04f3f2f034156cb
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-11-19 09:45:25 +0100

    reverted types change. Tests don't fail anymore.

--- cate/core/types.py
+++ cate/core/types.py
@@ -804,15 +804,7 @@ class GeoDataFrame:
         features = self._features
         if features is not None and self._lazy_data_frame is None:
             crs = features.crs if hasattr(features, 'crs') else None
-            df = geopandas.GeoDataFrame.from_features(features, crs=crs)
-            cols = df.columns.tolist()
-            if 'geometry' in cols and cols.index('geometry') != (len(cols) - 1):
-                cols = set(cols) - {'geometry', }
-                cols = list(cols) + ['geometry', ]
-
-                self._lazy_data_frame = df[cols]
-            else:
-                self._lazy_data_frame = df
+            self._lazy_data_frame = geopandas.GeoDataFrame.from_features(features, crs=crs)
 
         return self._lazy_data_frame
 
