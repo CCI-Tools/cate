4d05917c4777a0561e1d949a66f337166af32296
suvarchal <suvarchal.kumar@gmail.com>
2020-03-05 12:27:51 +0100

    add test for changing root url path

--- tests/webapi/test_start.py
+++ tests/webapi/test_start.py
@@ -1,6 +1,7 @@
 import json
 import os
 import unittest
+from unittest.mock import patch
 
 from tornado.testing import AsyncHTTPTestCase
 from cate.webapi.start import create_application
@@ -13,13 +14,24 @@ NETCDF_TEST_FILE = os.path.join(os.path.dirname(__file__), '..', 'data', 'precip
 @unittest.skipIf(os.environ.get('CATE_DISABLE_WEB_TESTS', None) == '1', 'CATE_DISABLE_WEB_TESTS = 1')
 class WebAPITest(AsyncHTTPTestCase):
     def get_app(self):
+        self.url = '/'
         return create_application(user_root_path=None)
 
     def test_base_url(self):
-        response = self.fetch('/')
+        response = self.fetch(self.url)
         self.assertEqual(response.code, 200)
         json_dict = json.loads(response.body.decode('utf-8'))
         self.assertIn('status', json_dict)
         self.assertIn('content', json_dict)
         self.assertIn('name', json_dict['content'])
         self.assertIn('version', json_dict['content'])
+
+
+# Tests if root of url can be changed, mostly relevant for CateHub context.
+
+@unittest.skipIf(os.environ.get('CATE_DISABLE_WEB_TESTS', None) == '1', 'CATE_DISABLE_WEB_TESTS = 1')
+class WebAPIHubContextTest(WebAPITest):
+    @patch.dict(os.environ, {'JUPYTERHUB_SERVICE_PREFIX': '/user/test/'})
+    def get_app(self):
+        self.url = os.environ.get('JUPYTERHUB_SERVICE_PREFIX')
+        return create_application(user_root_path=None)
